8acce249e5a0ad85eb5424d7ce4d56eaa86d0c27
davidtrowbridge <davidtrowbridge@08b3d480-bf2c-0410-a26f-811ee3361c24>
2002-07-29 23:16:46 +0000

    Initial revision
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@809 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ web/screenshots/index.php
@@ -0,0 +1,23 @@
+<?
+include "../document.php";
+
+$doc = new Document;
+$doc->begin("screenshots");
+print <<< end
+These are some screenshots of the up-and-coming 1.8 version. Soon we'll get
+some shots from the current stable version, 1.7e6.<br><br><br>
+<a href="bzfi0000.jpg"><img src="t-bzfi0000.jpg" border="0" alt="screenshot 0000"></a>
+<a href="bzfi0001.jpg"><img src="t-bzfi0001.jpg" border="0" alt="screenshot 0001"></a>
+<a href="bzfi0002.jpg"><img src="t-bzfi0002.jpg" border="0" alt="screenshot 0002"></a>
+<a href="bzfi0003.jpg"><img src="t-bzfi0003.jpg" border="0" alt="screenshot 0003"></a>
+<a href="bzfi0004.jpg"><img src="t-bzfi0004.jpg" border="0" alt="screenshot 0004"></a>
+<a href="bzfi0005.jpg"><img src="t-bzfi0005.jpg" border="0" alt="screenshot 0005"></a>
+<a href="bzfi0006.jpg"><img src="t-bzfi0006.jpg" border="0" alt="screenshot 0006"></a>
+<a href="bzfi0007.jpg"><img src="t-bzfi0007.jpg" border="0" alt="screenshot 0007"></a>
+<a href="bzfi0008.jpg"><img src="t-bzfi0008.jpg" border="0" alt="screenshot 0008"></a>
+<a href="bzfi0009.jpg"><img src="t-bzfi0009.jpg" border="0" alt="screenshot 0009"></a>
+<a href="bzfi0010.jpg"><img src="t-bzfi0010.jpg" border="0" alt="screenshot 0010"></a>
+end;
+$doc->end();
+
+?>
