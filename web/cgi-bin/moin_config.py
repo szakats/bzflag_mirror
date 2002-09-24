8cd94d97265c1b0c804b287a7b33fa7d41e320aa
timriker <timriker@08b3d480-bf2c-0410-a26f-811ee3361c24>
2002-09-24 08:53:44 +0000

    dl and license
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@841 08b3d480-bf2c-0410-a26f-811ee3361c24

--- web/cgi-bin/moin_config.py
+++ web/cgi-bin/moin_config.py
@@ -47,6 +47,7 @@ title1 = """<div align="center">
 <a href="/help/" class="navbar">help</a><br>
 <a href="/developers/" class="navbar">developers</a><br>
 <a href="/screenshots/" class="navbar">screenshots</a><br>
+<a href="http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/bzflag/bzflag/LICENSE?rev=HEAD&content-type=text/vnd.viewcvs-markup" class="navbar">license</a><br>
 <a href="/getin/" class="navbar">get&nbsp;involved!</a><br>
 <a href="/cgi-bin/moin.cgi/OtherLinks" class="navbar">links</a><br>
 <a href="/cgi-bin/moin.cgi/" class="navbar">wiki</a><br>
@@ -56,6 +57,8 @@ title1 = """<div align="center">
 <a href="http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/bzflag" class="navbar">&nbsp;browse&nbsp;CVS</a><br>
 <a href="http://sourceforge.net/tracker/?group_id=3248&atid=103248" class="navbar">&nbsp;bug&nbsp;reports</a><br>
 <a href="http://sourceforge.net/cvs/?group_id=3248" class="navbar">&nbsp;CVS&nbsp;access</a><br>
+<a href="http://sourceforge.net/project/showfiles.php?group_id=3248" class="navbar">&nbsp;downloads</a><br>
+<a href="http://ftp1.sourceforge.net/bzflag/" class="navbar">&nbsp;download&nbsp;dir</a><br>
 <a href="http://sourceforge.net/tracker/?atid=353248&group_id=3248&func=browse" class="navbar">&nbsp;feature&nbsp;requests</a><br>
 <a href="http://sourceforge.net/forum/?group_id=3248" class="navbar">&nbsp;forums</a><br>
 <a href="http://sourceforge.net/mail/?group_id=3248" class="navbar">&nbsp;mailing&nbsp;lists</a><br>
