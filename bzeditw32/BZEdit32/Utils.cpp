530538418c1e106eeaef26c93eec94e23411775e
jeffm2501 <jeffm2501@08b3d480-bf2c-0410-a26f-811ee3361c24>
2003-06-30 17:35:45 +0000

    *** empty log message ***
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@1695 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ bzeditw32/BZEdit32/Utils.cpp
@@ -0,0 +1,93 @@
+// utils.cpp
+
+#include "Utils.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+//loger
+void LogLine ( char* szLine )
+{
+	char cr[3] = {10,13,0};
+
+	if (!szLine)
+		return;
+
+	FILE	*fpLog = fopen ("log.txt","at");
+
+	if(!fpLog)
+		return;
+
+	fprintf(fpLog,"%s\n",szLine);
+	fclose(fpLog);
+}
+
+// util funcs
+bool GetLine( char** data, char* line )
+{
+	line[0] = NULL;
+	char	*pPtr = *data, *pOut = line;
+
+	while (*pPtr != '\n')
+	{
+		*pOut= *pPtr;
+		if (*pPtr == NULL)
+			return false;
+
+		pPtr++;
+		pOut++;
+	}
+	pPtr++;
+	pOut;
+
+	*data = pPtr;
+	*pOut = NULL;
+	return true;
+}
+
+bool GetNextElement ( char** pData, char* pElemnt, char* pName )
+{
+	if (!pData || !pElemnt)
+		return false;
+	
+	pElemnt[0] = NULL;
+
+	char	*pIn = *pData,*pOut = pElemnt,line[255];
+
+	bool bDone = false,bEOF = false;
+
+	while (!bDone)
+	{
+		if (!GetLine(&pIn,line))
+		{
+			bDone = true;
+			bEOF = true;
+		}
+		switch (line[0])
+		{
+			case '\n':
+			case '\t':
+			case ' ':
+			case NULL:
+				break;
+
+			case '#':
+				if (pElemnt[0]== NULL) 
+					break;
+
+				if (line[1]!= '!')
+					break;
+			default:
+				strcat(pElemnt,line);
+				
+				if (stricmp(line,"end")==0)
+					bDone = true;
+
+				strcat(pElemnt,"\n");
+				break;
+		}
+	}
+
+	*pData = pIn;
+	return bEOF;
+}
---
+++ bzeditw32/BZEdit32/Utils.h
@@ -0,0 +1,9 @@
+// utils.h
+
+#ifndef _UTILS_H_
+#define _UTILS_H_
+
+void LogLine ( char* szLine );
+bool GetLine( char** data, char* line );
+bool GetNextElement ( char** pData, char* pElemnt, char* pName );
+#endif //_UTILS_H_
