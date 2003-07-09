cbc2b99f0928f61ac7528c427a86c145e18d0172
micahjd <micahjd@08b3d480-bf2c-0410-a26f-811ee3361c24>
2003-07-09 08:16:26 +0000

    Add MsgTeleport to ToServer
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@2060 08b3d480-bf2c-0410-a26f-811ee3361c24

--- pybzflag/BZFlag/Protocol/ToServer.py
+++ pybzflag/BZFlag/Protocol/ToServer.py
@@ -107,6 +107,14 @@ class MsgUDPLinkEstablished(Common.Message):
         StructEntry(UInt16, 'queueUpdate'),
         ]
 
+class MsgTeleport(Common.Message):
+    messageId = 0x7470
+    entries = [
+        StructEntry(UInt16,          'fromId'),
+        StructEntry(UInt16,          'toId'),
+        ]
+
+
 MsgUDPLinkRequest = Common.MsgUDPLinkRequest
 MsgNegotiateFlags = Common.MsgNegotiateFlags
 MsgShotBegin = Common.MsgShotBegin
