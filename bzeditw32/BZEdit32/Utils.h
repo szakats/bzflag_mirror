5f0220dfa3efee50cfd72c8d23d5517f812415bb
(no author) <(no author)@08b3d480-bf2c-0410-a26f-811ee3361c24>
2003-07-09 04:02:42 +0000

    This commit was manufactured by cvs2svn to create branch 'remove_flag_id'.
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/branches/remove_flag_id@2019 08b3d480-bf2c-0410-a26f-811ee3361c24

--- bzeditw32/BZEdit32/Utils.h
+++
@@ -1,9 +0,0 @@
-// utils.h
-
-#ifndef _UTILS_H_
-#define _UTILS_H_
-
-void LogLine ( char* szLine );
-bool GetLine( char** data, char* line );
-bool GetNextElement ( char** pData, char* pElemnt, char* pName );
-#endif //_UTILS_H_
