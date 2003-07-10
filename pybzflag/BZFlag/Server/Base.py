d0dddc3c9e2a13d20ed0088b99352cc13db78c3c
micahjd <micahjd@08b3d480-bf2c-0410-a26f-811ee3361c24>
2003-07-10 02:47:45 +0000

    Split up server classes into separate files
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@2105 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ pybzflag/BZFlag/Server/Base.py
@@ -0,0 +1,100 @@
+""" BZFlag.Server.Base
+
+Provides the BaseServer class, which implements basic communication
+with clients and provides hooks for adding more functionality
+in subclasses.
+"""
+#
+# Python BZFlag Protocol Package
+# Copyright (C) 2003 Micah Dowty <micahjd@users.sourceforge.net>
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2.1 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+import BZFlag
+from BZFlag import Network, Protocol, Event, Errors
+
+
+class BaseServer(Network.Endpoint):
+    """On top of the basic message processing provided by Network.Endpoint,
+       this class manages connections from clients.
+       """
+
+    outgoing = Protocol.FromServer
+    incoming = Protocol.ToServer
+    
+    def init(self):
+        Network.Endpoint.init(self)
+        self.clients = {}
+        self.nextClientID = 0
+        self.clientIDIncrement = 1
+        self.options.update({
+            'interface': None,
+            })
+        Event.attach(self, 'onConnect', 'onDisconnect')
+
+        def setOptions(**options):
+            if 'interface' in options.keys():
+                self.listen(options['interface'])
+        self.onSetOptions.observe(setOptions)
+
+    def listen(self, interface):
+        self.tcp = Network.Socket()
+        self.tcp.listen(interface, Protocol.Common.defaultPort)
+        self.tcp.setBlocking(0)
+        self.tcp.handler = self.handleConnection
+        self.eventLoop.add(self.tcp)
+
+    def getNewClientID(self):
+        id = self.nextClientID
+        self.nextClientID += self.clientIDIncrement
+        return id
+
+    def handleConnection(self, socket, eventLoop):
+        """This is called when a new connection to the server is made.
+           It sends the hello packet, and sets up a new client socket.
+           """
+        # On a new client connection, send them the
+        # hello packet and add to our dict of clients.
+        clientSocket = socket.accept()
+        clientId = self.getNewClientID()
+        clientSocket.id = clientId
+        self.clients[clientId] = clientSocket
+        hello = self.outgoing.HelloPacket(version = BZFlag.protocolVersion,
+                                          clientId = clientId)
+        self.eventLoop.add(clientSocket)
+        clientSocket.setBlocking(0)
+        clientSocket.handler = self.handleMessage
+        clientSocket.write(hello)
+        self.onConnect(clientSocket)
+
+    def handleMessage(self, socket, eventLoop):
+        """This is called when new data is received from the client.
+           It is a wrapper around Network.Endpoint.handleMessage which
+           catches ConnectionLost exceptions and disconnects the client
+           safely.
+           """
+        try:
+            Network.Endpoint.handleMessage(self, socket, eventLoop)
+        except Errors.ConnectionLost:
+            try:
+                del self.clients[socket.id]
+                self.eventLoop.remove(socket)
+                self.onDisconnect(socket)
+            except KeyError:
+                # Already disconnected
+                pass
+
+### The End ###
--- pybzflag/BZFlag/Server/Stateful.py
+++ pybzflag/BZFlag/Server/Stateful.py
@@ -1,8 +1,7 @@
-""" BZFlag.Server
+""" BZFlag.Server.Stateful
 
-Provides the BaseServer class, which implements basic communication
-with clients and provides hooks for adding more functionality
-in subclasses.
+Provides the StatefulServer class, which extends BaseServer to
+support updating a game state and transmitting changes.
 """
 #
 # Python BZFlag Protocol Package
@@ -24,79 +23,9 @@ in subclasses.
 #
 
 import BZFlag
-from BZFlag import Network, Protocol, Event, Errors, Game, Flag
+from BZFlag import Event, Game, Flag
 from StringIO import StringIO
-
-
-class BaseServer(Network.Endpoint):
-    """On top of the basic message processing provided by Network.Endpoint,
-       this class manages connections from clients.
-       """
-
-    outgoing = Protocol.FromServer
-    incoming = Protocol.ToServer
-    
-    def init(self):
-        Network.Endpoint.init(self)
-        self.clients = {}
-        self.nextClientID = 0
-        self.clientIDIncrement = 1
-        self.options.update({
-            'interface': None,
-            })
-        Event.attach(self, 'onConnect', 'onDisconnect')
-
-        def setOptions(**options):
-            if 'interface' in options.keys():
-                self.listen(options['interface'])
-        self.onSetOptions.observe(setOptions)
-
-    def listen(self, interface):
-        self.tcp = Network.Socket()
-        self.tcp.listen(interface, Protocol.Common.defaultPort)
-        self.tcp.setBlocking(0)
-        self.tcp.handler = self.handleConnection
-        self.eventLoop.add(self.tcp)
-
-    def getNewClientID(self):
-        id = self.nextClientID
-        self.nextClientID += self.clientIDIncrement
-        return id
-
-    def handleConnection(self, socket, eventLoop):
-        """This is called when a new connection to the server is made.
-           It sends the hello packet, and sets up a new client socket.
-           """
-        # On a new client connection, send them the
-        # hello packet and add to our dict of clients.
-        clientSocket = socket.accept()
-        clientId = self.getNewClientID()
-        clientSocket.id = clientId
-        self.clients[clientId] = clientSocket
-        hello = self.outgoing.HelloPacket(version = BZFlag.protocolVersion,
-                                          clientId = clientId)
-        self.eventLoop.add(clientSocket)
-        clientSocket.setBlocking(0)
-        clientSocket.handler = self.handleMessage
-        clientSocket.write(hello)
-        self.onConnect(clientSocket)
-
-    def handleMessage(self, socket, eventLoop):
-        """This is called when new data is received from the client.
-           It is a wrapper around Network.Endpoint.handleMessage which
-           catches ConnectionLost exceptions and disconnects the client
-           safely.
-           """
-        try:
-            Network.Endpoint.handleMessage(self, socket, eventLoop)
-        except Errors.ConnectionLost:
-            try:
-                del self.clients[socket.id]
-                self.eventLoop.remove(socket)
-                self.onDisconnect(socket)
-            except KeyError:
-                # Already disconnected
-                pass
+from BZFlag.Server.Base import BaseServer
 
 
 class StatefulServer(BaseServer):
@@ -212,12 +141,5 @@ class StatefulServer(BaseServer):
             fromId = 'server',
             toId = msg.socket.id,
             message = self.options['welcomeMessage']))
- 
-
-class StandardServer(StatefulServer):
-    """A server that implements standard game rules in the world supplied
-       by a StatefulServer
-       """
-    pass
 
 ### The End ###
