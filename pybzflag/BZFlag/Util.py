62d64a4cc0d8de9d3433f36be1763dbf4a7127c2
micahjd <micahjd@08b3d480-bf2c-0410-a26f-811ee3361c24>
2003-07-11 07:45:22 +0000

    Moved proxy code to Util. Now traceproxy is only command line processing and message filtering code
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@2197 08b3d480-bf2c-0410-a26f-811ee3361c24

--- pybzflag/BZFlag/Util.py
+++ pybzflag/BZFlag/Util.py
@@ -133,4 +133,36 @@ def messageDump(msg, f, showContents=True, keyColumnWidth=25):
             if line:
                 f.write(" " * (keyColumnWidth + 2) + line + "\n")
 
+
+def proxy(server, client, callback):
+    """Set up a proxy between the server and client.
+       Currently this only works correctly when there is one client
+       connected to the server. It forwards all events (tcp and udp)
+       from one to the other, optionally calling the given callback
+       for each message.
+       """
+    # Let the user know when our client connects, and force
+    # our server to hand out the same client ID we were given.
+    def onClientConnect():
+        server.nextClientID = client.id
+        server.clientIDIncrement = 0
+    client.onConnect.observe(onClientConnect)
+
+    # Set up events to forward messages between
+    # client and server, dumping them to stdout
+    def onClientMessage(msg):
+        try:
+            server.clientsByID[client.id].write(msg, msg.protocol)
+            if callback:
+                callback(msg)
+        except KeyError:
+            pass
+    client.onUnhandledMessage.replace(onClientMessage)
+
+    def onServerMessage(msg):
+        getattr(client, msg.protocol).write(msg)
+        if callback:
+            callback(msg)
+    server.onUnhandledMessage.replace(onServerMessage)
+
 ### The End ###
--- pybzflag/traceproxy.py
+++ pybzflag/traceproxy.py
@@ -3,7 +3,7 @@
 # A utility that acts as a BZFlag proxy server, showing all
 # messages in a human readable form.
 #
-from BZFlag import CommandLine, Server, Client, Event, Protocol, Util
+from BZFlag import CommandLine, Event, Server, Client, Util
 import sys
 
 # Create a server and a client, sharing command line options and event loop
@@ -19,23 +19,16 @@ argParser.add_option("-o", "--show", action="append", dest="show",
 (server, client) = argParser.parse()
 options = client.cmdLineValues
 
+# Trace client connection
+client.onConnect.trace(lambda: "--- Client connected to %s\n--- Client ID is %d" %
+                       (client.options['server'], client.id))
 
 # Trace server connections and disconnections
 print "--- Server is listening on %s:%d" % (server.tcp.interface, server.tcp.port)
-server.onConnect.trace(lambda socket: "*** Connected client %d, from %s:%s" %
+server.onConnect.trace(lambda socket: "--- Connected client %d, from %s:%s" %
                        (socket.id, socket.address[0], socket.address[1]))
-server.onDisconnect.trace(lambda socket: "*** Disconnected client %d, from %s:%s" %
+server.onDisconnect.trace(lambda socket: "--- Disconnected client %d, from %s:%s" %
                           (socket.id, socket.address[0], socket.address[1]))
-
-
-# Let the user know when our client connects, and force
-# our server to hand out the same client ID we were given.
-def onClientConnect():
-    print "*** Connected to server %s" % client.options['server']
-    print "--- Client ID is %d" % client.id
-    server.nextClientID = client.id
-    server.clientIDIncrement = 0
-client.onConnect.observe(onClientConnect)
 
 # Dump the message using Util.messageDump, filtering it according to our options
 def showMessage(msg):
@@ -46,19 +39,8 @@ def showMessage(msg):
         return
     Util.messageDump(msg, sys.stdout, not options['names'])
 
-# Set up events to forward messages between
-# client and server, dumping them to stdout
-def onClientMessage(msg):
-    try:
-        server.clientsByID[client.id].write(msg, msg.protocol)
-        showMessage(msg)
-    except KeyError:
-        pass
-client.onUnhandledMessage.replace(onClientMessage)
-
-def onServerMessage(msg):
-    getattr(client, msg.protocol).write(msg)
-    showMessage(msg)
-server.onUnhandledMessage.replace(onServerMessage)
+# Set up a proxy between the client and server,
+# with showMessage as a callback for each message
+Util.proxy(server, client, showMessage)
 
 eventLoop.run()
