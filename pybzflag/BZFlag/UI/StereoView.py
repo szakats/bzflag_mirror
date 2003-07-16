2e943f71584f7c0d242bd6dec622b31d819e6170
micahjd <micahjd@08b3d480-bf2c-0410-a26f-811ee3361c24>
2003-07-16 07:17:51 +0000

    Yay, stereo
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@2474 08b3d480-bf2c-0410-a26f-811ee3361c24

--- pybzflag/BZFlag/UI/StereoView.py
+++ pybzflag/BZFlag/UI/StereoView.py
@@ -26,6 +26,23 @@ from BZFlag.UI import Viewport, ThreeDView, ThreeDControl, ThreeDRender, Layout,
 import BZFlag, math
 from OpenGL.GL import *
 
+
+class RightCamera(ThreeDRender.Camera):
+    """The left eye has our 'master camera' that is operated by the ThreeDControl.Viewing
+       instance. This class replaces the camera for the right eye, and manages setting the
+       azimuthOffset correctly when switching between views.
+       """
+    def __init__(self, left, eyesep=12.5):
+        self.left = left
+        self.eyesep = eyesep
+
+    def load(self):
+        angle = math.atan(self.eyesep / self.left.distance)
+        self.left.azimuthOffset = angle
+        self.left.load()
+        self.left.azimuthOffset = -angle
+
+        
 class StereoView:
     """Shows a side-by-side stereo view of the BZFlag game, renderable
        to an OpenGLViewport.
@@ -34,19 +51,8 @@ class StereoView:
         self.lefteye  = ThreeDView.ThreeDView(game, viewport.region(Layout.Rect(viewport).left(0.5)))
         self.righteye = ThreeDView.ThreeDView(game, viewport.region(Layout.Rect(viewport).right(0.5)))
         ThreeDControl.Viewing(self.lefteye, viewport)
-        self.righteye.camera = self.lefteye.camera
-        viewport.onDrawFrame.observe(self.render)
+        self.righteye.camera = RightCamera(self.lefteye.camera, 300)
 
-    def render(self):
-#        eyesep = 12.5
-        eyesep = 300
-        angle = math.atan(eyesep / self.lefteye.camera.distance)
-        # draw left eye
-        self.lefteye.camera.azimuthOffset = -angle
-        self.lefteye.render()
-        # draw right eye
-        self.righteye.camera.azimuthOffset = angle
-        self.righteye.render()
 
 def attach(game, eventLoop):
     viewport = Viewport.OpenGLViewport(eventLoop, (1600, 600))
