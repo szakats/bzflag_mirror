4e645fa7114f4da2ff6a82a0d2c086b38a100f2c
micahjd <micahjd@08b3d480-bf2c-0410-a26f-811ee3361c24>
2003-07-18 00:08:22 +0000

    Fold stereo and anaglyph modes into one modelviewer, with command line options
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@2528 08b3d480-bf2c-0410-a26f-811ee3361c24

--- pybzflag/modelviewer.py
+++ pybzflag/modelviewer.py
@@ -3,28 +3,47 @@
 # Simple viewer for the 3D models PyBZFlag uses, in a subset of the VRML format.
 # Give the file name or URL to a .wrl model on the command line.
 #
-from BZFlag.UI import Viewport, ThreeDRender, ThreeDControl
+from BZFlag.UI import Viewport, ThreeDRender, ThreeDControl, AnaglyphView, StereoView
 from BZFlag.UI.Drawable import Box, VRML
-from BZFlag.Event import EventLoop
+from BZFlag import Event, optik
 import sys, os
 
+
+# Command line processing
+parser = optik.OptionParser(usage = "usage: %prog [options] model.wrl")
+parser.add_option("-a", "--anaglyph", action="store_true",
+                  help="Enables anaglyph stereo mode.")
+parser.add_option("-s", "--stereo", action="store_true",
+                  help="Enables dual-channel stereo mode.")
+(options, args) = parser.parse_args()
+
 try:
-    fileName = sys.argv[1]
+    fileName = args[0]
 except IndexError:
-    print "A model filename or URI must be specified on the command line."
+    print "A model filename must be specified on the command line."
     sys.exit(0)
 
+if options.anaglyph:
+    viewClass = AnaglyphView.AnaglyphView
+elif options.stereo:
+    viewClass = StereoView.StereoView    
+else:
+    viewClass = ThreeDRender.View
+    
+
 # Set up a quick 3D renderer view with a controller attached so we can spin the model around
-loop = EventLoop()
+loop = Event.EventLoop()
 viewport = Viewport.OpenGLViewport(loop, (800,600))
 viewport.setCaption("Model Viewer - %s" % fileName)
-view = ThreeDRender.View(viewport)
+view = viewClass(viewport)
 control = ThreeDControl.Viewing(view, viewport)
+
 
 # Move the camera a little closer in, and down to the origin
 view.camera.position = (0,0,0)
 view.camera.distance = 60
 view.camera.jump()
+
 
 # Read in the VRML file, putting each mesh in a separate object
 meshes = VRML.load(os.path.abspath(fileName))
@@ -36,11 +55,13 @@ class MeshObject:
 for name, drawable in meshes.items():
     view.scene.add(MeshObject(name), [drawable])
 
+
 # Let the user pick objects with the left mouse button, printing the mesh name
 def onMouseButtonDown(event):
     if event.button == 1:
         print 'picked: %r' % view.pick(event.pos)
 viewport.onMouseButtonDown.observe(onMouseButtonDown)
+
 
 view.scene.preprocess()
 loop.run()
