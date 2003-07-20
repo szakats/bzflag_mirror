bae19075a022e1d1638361493955eba11cb5adf5
micahjd <micahjd@08b3d480-bf2c-0410-a26f-811ee3361c24>
2003-07-20 03:50:39 +0000

    More texture tweaking. Removes the old concrete_dark and lighted_girder textures that sucked, adds a new platform edge texture that's a good bit better. Beginning the process of fixing up the box tops.
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@2581 08b3d480-bf2c-0410-a26f-811ee3361c24

--- pybzflag/BZFlag/UI/Drawable/Box.py
+++ pybzflag/BZFlag/UI/Drawable/Box.py
@@ -32,33 +32,42 @@ import random
 
 class BoxSides(DisplayList):
     def __init__(self, box):
+        # Default wall
+        # This is brick, so we want to make sure we don't end up
+        # with a fractional number of bricks. This brick texture has
+        # 5 bricks horizontally and 10 vertically.
+        self.textureNames = ["brick.jpeg"]
+        numBricks = (int(box.size[0]),
+                     int(box.size[1]),
+                     int(box.size[2] * 2))
+        self.texRepeats = (numBricks[0] / 5,
+                           numBricks[1] / 5,
+                           numBricks[2] / 10)
+        self.tex2Repeats = (box.size[0] / 2,
+                            box.size[1] / 2,
+                            1)
+
         # Bridges and platforms
         if box.size[2] < 10 and box.center[2] > 1:
-            self.textureNames = ["lighted_girder.jpeg"]
-            aspect = 0.6489
-            self.texRepeats = (box.size[0] / (box.size[2] * aspect),
-                               box.size[1] / (box.size[2] * aspect),
+            self.textureNames = ["platform_side.jpeg", "wall_grime.jpeg"]
+            aspect = 8/10
+            self.texRepeats = (box.size[0] / box.size[2] * 2 * aspect,
+                               box.size[1] / box.size[2] * 2 * aspect,
                                1)
-
-        # Default wall
-        else:
-            # This is brick, so we want to make sure we don't end up
-            # with a fractional number of bricks. This brick texture has
-            # 5 bricks horizontally and 10 vertically.
-            self.textureNames = ["brick.jpeg"]
-            numBricks = (int(box.size[0]),
-                         int(box.size[1]),
-                         int(box.size[2] * 2))
-            self.texRepeats = (numBricks[0] / 5,
-                               numBricks[1] / 5,
-                               numBricks[2] / 10)
-
+            self.tex2Repeats = (box.size[0] / 4,
+                                box.size[1] / 4,
+                                1)
+        
         DisplayList.__init__(self, box)
                
     def set(self, box):
         self.center = box.center
         self.angle = box.angle
         self.size = box.size
+        try:
+            self.render.textures[1].texEnv = GL_MODULATE
+        except IndexError:
+            pass
 
     def drawToList(self):
         glPushMatrix()
@@ -68,42 +77,58 @@ class BoxSides(DisplayList):
         # Y+ side
         glNormal3f(0, 1, 0)
         glTexCoord2f(0, 0)
+        glMultiTexCoord2fARB(GL_TEXTURE1_ARB, 0, 0)
         glVertex3f(-self.size[0], self.size[1], 0)
         glTexCoord2f(0, self.texRepeats[2])
+        glMultiTexCoord2fARB(GL_TEXTURE1_ARB, 0, self.tex2Repeats[2])
         glVertex3f(-self.size[0], self.size[1], self.size[2])
         glTexCoord2f(self.texRepeats[0], self.texRepeats[2])
+        glMultiTexCoord2fARB(GL_TEXTURE1_ARB, self.tex2Repeats[0], self.tex2Repeats[2])
         glVertex3f(self.size[0], self.size[1], self.size[2])
         glTexCoord2f(self.texRepeats[0], 0)
+        glMultiTexCoord2fARB(GL_TEXTURE1_ARB, self.tex2Repeats[0], 0)
         glVertex3f(self.size[0], self.size[1], 0)
         # Y- side
         glNormal3f(0, -1, 0)
         glTexCoord2f(0, 0)
+        glMultiTexCoord2fARB(GL_TEXTURE1_ARB, 0, 0)
         glVertex3f(self.size[0], -self.size[1], 0)
         glTexCoord2f(0, self.texRepeats[2])
+        glMultiTexCoord2fARB(GL_TEXTURE1_ARB, 0, self.tex2Repeats[2])
         glVertex3f(self.size[0], -self.size[1], self.size[2])
         glTexCoord2f(self.texRepeats[0], self.texRepeats[2])
+        glMultiTexCoord2fARB(GL_TEXTURE1_ARB, self.tex2Repeats[0], self.tex2Repeats[2])
         glVertex3f(-self.size[0], -self.size[1], self.size[2])
         glTexCoord2f(self.texRepeats[0], 0)
+        glMultiTexCoord2fARB(GL_TEXTURE1_ARB, self.tex2Repeats[0], 0)
         glVertex3f(-self.size[0], -self.size[1], 0)
         # X+ side
         glNormal3f(1, 0, 0)
         glTexCoord2f(0, 0)
+        glMultiTexCoord2fARB(GL_TEXTURE1_ARB, 0, 0)
         glVertex3f(self.size[0], self.size[1], 0)
         glTexCoord2f(0, self.texRepeats[2])
+        glMultiTexCoord2fARB(GL_TEXTURE1_ARB, 0, self.tex2Repeats[2])
         glVertex3f(self.size[0], self.size[1], self.size[2])
         glTexCoord2f(self.texRepeats[1], self.texRepeats[2])
+        glMultiTexCoord2fARB(GL_TEXTURE1_ARB, self.tex2Repeats[1], self.tex2Repeats[2])
         glVertex3f(self.size[0], -self.size[1], self.size[2])
         glTexCoord2f(self.texRepeats[1], 0)
+        glMultiTexCoord2fARB(GL_TEXTURE1_ARB, self.tex2Repeats[1], 0)
         glVertex3f(self.size[0], -self.size[1], 0)
         # X- side
         glNormal3f(-1, 0, 0)
         glTexCoord2f(0, 0)
+        glMultiTexCoord2fARB(GL_TEXTURE1_ARB, 0, 0)
         glVertex3f(-self.size[0], -self.size[1], 0)
         glTexCoord2f(0, self.texRepeats[2])
+        glMultiTexCoord2fARB(GL_TEXTURE1_ARB, 0, self.tex2Repeats[2])
         glVertex3f(-self.size[0], -self.size[1], self.size[2])
         glTexCoord2f(self.texRepeats[1], self.texRepeats[2])
+        glMultiTexCoord2fARB(GL_TEXTURE1_ARB, self.tex2Repeats[1], self.tex2Repeats[2])
         glVertex3f(-self.size[0], self.size[1], self.size[2])
         glTexCoord2f(self.texRepeats[1], 0)
+        glMultiTexCoord2fARB(GL_TEXTURE1_ARB, self.tex2Repeats[1], 0)
         glVertex3f(-self.size[0], self.size[1], 0)
         glEnd()
         glPopMatrix()
@@ -112,7 +137,7 @@ class BoxSides(DisplayList):
 class BoxTop(DisplayList):
     def __init__(self, box):
         self.box = box
-        self.textureNames = ['concrete_dark.jpeg', 'lightmap.jpeg']
+        self.textureNames = ['concrete.jpeg']
         self.tex2Coords = ( (0,0), (1,0), (1,1), (0,1) )
 
         # If the box is fairly large and squareish, use a square oil-stain texture
@@ -137,7 +162,6 @@ class BoxTop(DisplayList):
 
         try:
             self.render.textures[1].texEnv = GL_MODULATE
-            self.render.textures[2].texEnv = GL_MODULATE
         except IndexError:
             pass
 
@@ -148,10 +172,9 @@ class BoxTop(DisplayList):
         glBegin(GL_POLYGON)
         for i in xrange(4):
             vertex = self.polygon[i]
-            glTexCoord2f(vertex[0] / 30, vertex[1] / 30)
+            glTexCoord2f(vertex[0] / 20, vertex[1] / 20)
             tex2Coord = self.tex2Coords[i]
-            glMultiTexCoord2fARB(GL_TEXTURE1_ARB, vertex[0] / 400, vertex[1] / 400)
-            glMultiTexCoord2fARB(GL_TEXTURE2_ARB, tex2Coord[0], tex2Coord[1])
+            glMultiTexCoord2fARB(GL_TEXTURE1_ARB, tex2Coord[0], tex2Coord[1])
             glVertex2f(*vertex)
         glEnd()
         glPopMatrix()
@@ -159,7 +182,7 @@ class BoxTop(DisplayList):
 
 class BoxBottom(DisplayList):
     def __init__(self, box):
-        self.textureNames = ['concrete_light.jpeg']
+        self.textureNames = ['concrete.jpeg']
         DisplayList.__init__(self, box)
                
     def set(self, box):
