742b1c9340474ff6462af66caec6682d2e567a7d
micahjd <micahjd@08b3d480-bf2c-0410-a26f-811ee3361c24>
2003-07-25 06:32:31 +0000

    A python class to do RGB to YUV420 color conversion with Numeric. Part of the effort to port the Jetstream/Crystalspace movie recorder to Python.
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@2723 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ pybzflag/BZFlag/UI/MovieRecorder.py
@@ -0,0 +1,89 @@
+""" BZFlag.UI.MovieRecorder
+
+A mostly-real-time movie recorder that saves to NuppelVideo files.
+This is based on the recorder I originally wrote for Jetstream and
+later ported to Crystal Space.
+"""
+#
+# Python BZFlag Protocol Package
+# Copyright (C) 2003 Micah Dowty <micahjd@users.sourceforge.net>
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2.1 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+from Numeric import *
+
+
+class RGBtoYUV420:
+    """Encapsulation for the RGB to YUV420 colorspace converter.
+       This must be a class because it requires lookup tables.
+       This is ported from rgb2yuv420.cpp in the jetstream/crystalspace
+       movie recorder.
+
+       YUV420 is a planar format, where the U and V planes are half the
+       resolution (1/4 the number of pixels) of the Y plane. This reduces
+       spacial color resolution.
+       """
+    def __init__(self):
+        self.yr =   zeros(256, UInt32, savespace=True)
+        self.yg   = zeros(256, UInt32, savespace=True)
+        self.yb   = zeros(256, UInt32, savespace=True)
+        self.ur   = zeros(256, UInt32, savespace=True)
+        self.ug   = zeros(256, UInt32, savespace=True)
+        self.vg   = zeros(256, UInt32, savespace=True)
+        self.vb   = zeros(256, UInt32, savespace=True)
+        self.ubvr = zeros(256, UInt32, savespace=True)
+
+        for i in xrange(256):
+            isl8 = i << 8
+            yr[i]   = 65.481  * isl8
+            yg[i]   = 128.553 * isl8 + 1048576
+            yb[i]   = 24.966  * isl8
+            ur[i]   = 37.797  * isl8
+            ug[i]   = 74.203  * isl8 + 8388608
+            vg[i]   = 93.786  * isl8 + 8388608
+            vb[i]   = 18.214  * isl8
+            ubvr[i] = 112     * isl8
+            
+    def convert(self, r, g, b):
+        """Accepts three Numeric arrays for the red, green, and blue planes.
+           Returns a tuple of 2-D nuemric arrays, for the Y, U, and V plane
+           respectively.
+           Note that for this to work right, the input width and height must
+           be even numbers, and each input plane must be the same size.
+           """
+        (width, height) = r.shape[:2]
+
+        # Use some fixed point math and lookup tables to calculate the luminance.
+        # No resampling is performed for the Y channel.
+        y = (take(self.yr, r) + take(self.yg, g) + take(self.yb, b)) >> 16
+
+        # Calculate the U and V planes, resampling the input down by 1/2
+        # Numeric's slice operator to the rescue!
+        u = ( -take(self.ur, r[ ::2, ::2]) -take(self.ug, g[ ::2, ::2]) +take(self.ubvr, b[ ::2, ::2])
+              -take(self.ur, r[1::2, ::2]) -take(self.ug, g[1::2, ::2]) +take(self.ubvr, b[1::2, ::2])
+              -take(self.ur, r[1::2,1::2]) -take(self.ug, g[1::2,1::2]) +take(self.ubvr, b[1::2,1::2])
+              -take(self.ur, r[ ::2,1::2]) -take(self.ug, g[ ::2,1::2]) +take(self.ubvr, b[ ::2,1::2]) ) >> 18
+
+        v = (  take(self.ubvr, r[ ::2, ::2]) -take(self.vg, g[ ::2, ::2]) -take(self.vb, b[ ::2, ::2])
+              +take(self.ubvr, r[1::2, ::2]) -take(self.vg, g[1::2, ::2]) -take(self.vb, b[1::2, ::2])
+              +take(self.ubvr, r[1::2,1::2]) -take(self.vg, g[1::2,1::2]) -take(self.vb, b[1::2,1::2])
+              +take(self.ubvr, r[ ::2,1::2]) -take(self.vg, g[ ::2,1::2]) -take(self.vb, b[ ::2,1::2]) ) >> 18
+        
+        # Cast everything back to 8 bits
+        return (y.astype(UInt8), u.astype(UInt8), v.astype(UInt8))
+            
+
+### The End ###
