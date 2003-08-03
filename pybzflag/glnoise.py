501e538a14f2e561f05dc441fea5a7348c32ab85
micahjd <micahjd@08b3d480-bf2c-0410-a26f-811ee3361c24>
2003-08-03 11:23:15 +0000

    More noise tweakage. Found a good value for the persistence using a keyboard interface for tweaking it in real time
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@2845 08b3d480-bf2c-0410-a26f-811ee3361c24

--- pybzflag/glnoise.py
+++ pybzflag/glnoise.py
@@ -35,6 +35,9 @@ class NoiseRenderer:
         self.scale = scale
         self.rotationDistance = rotationDistance
         self.rotationSpeed = rotationSpeed
+        self.reset()
+
+    def reset(self):
         self.angle = 0
 
     def render(self, texture, size):
@@ -79,14 +82,16 @@ class NoiseRenderer:
 
 if __name__ == '__main__':
     loop = EventLoop()
-    viewport = Viewport.OpenGLViewport(loop, (640,480), 1000)
+    viewport = Viewport.OpenGLViewport(loop, (800,600), 1000)
     viewport.setCaption("OpenGL Noise Experiment")
     viewport.fov = None   
 
-    r = NoiseRenderer()
+    r = NoiseRenderer()    
     octaves = [Texture.Texture() for i in xrange(9)]
+    persistence = 0.75
 
     def drawFrame():
+        r.reset()
         r.render(octaves[8], (2,2))
         r.render(octaves[7], (4,4))        
         r.render(octaves[6], (8,8))        
@@ -97,12 +102,8 @@ if __name__ == '__main__':
         r.render(octaves[1], (256,256))
         r.render(octaves[0], (512,512))
 
-        glClearColor(0.5, 0.5, 0.5, 1)
-        glClear(GL_COLOR_BUFFER_BIT)
-        
         glEnable(GL_TEXTURE_2D)
-        glEnable(GL_BLEND)
-        glColor4f(1,1,1,0.3)
+        glColor3f(1,1,1)
 
         for octave in octaves:
             octave.bind()
@@ -110,13 +111,25 @@ if __name__ == '__main__':
             glBegin(GL_QUADS)
             glTexCoord2f(0,0)
             glVertex2f(0,0)
-            glTexCoord2f(1,0)
+            glTexCoord2f(2,0)
             glVertex2f(viewport.size[0],0)
-            glTexCoord2f(1,1)
+            glTexCoord2f(2,2)
             glVertex2f(*viewport.size)
-            glTexCoord2f(0,1)
+            glTexCoord2f(0,2)
             glVertex2f(0,viewport.size[1])
             glEnd()
+
+            glEnable(GL_BLEND)
+            glColor4f(1,1,1,1-persistence)
+
+    def keyDown(event):
+        global persistence, r, seed
+        if event.unicode == '[':
+            persistence -= 0.05
+        elif event.unicode == ']':
+            persistence += 0.05
+        print "Persistence: %s" % persistence
+    viewport.onKeyDown.observe(keyDown)
 
     Util.showFrameRate(viewport)
     viewport.onDrawFrame.observe(drawFrame)
