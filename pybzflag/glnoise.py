b750653d3af625d6c2695f1c0978bf64f63f7194
micahjd <micahjd@08b3d480-bf2c-0410-a26f-811ee3361c24>
2003-08-03 14:09:01 +0000

    Tweak blending, remove cruft
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@2848 08b3d480-bf2c-0410-a26f-811ee3361c24

--- pybzflag/glnoise.py
+++ pybzflag/glnoise.py
@@ -14,7 +14,7 @@ time = Animated.Timekeeper()
 r = GLNoise.NoiseRenderer()    
 octaves = None
 
-persistence = 0.75
+persistence = 0.65
 period = 50
 
 def drawFrame():
@@ -32,53 +32,20 @@ def drawFrame():
                    )
 
     dt = time.step()
+    color = (1,1,1,1)
     for octave in octaves:
         octave.integrate(dt)
-        octave.draw(viewport.size, (1,1,1, 1-persistence))
+        octave.draw(viewport.size, color)
+        color = (1,1,1, 1-persistence)
 
-## octaves = [Texture.Texture() for i in xrange(9)]
-## persistence = 0.75
-
-## def drawFrame():
-##     r.reset()
-##     r.render(octaves[8], (2,2))
-##     r.render(octaves[7], (4,4))        
-##     r.render(octaves[6], (8,8))        
-##     r.render(octaves[5], (16,16))        
-##     r.render(octaves[4], (32,32))        
-##     r.render(octaves[3], (64,64))        
-##     r.render(octaves[2], (128,128))        
-##     r.render(octaves[1], (256,256))
-##     r.render(octaves[0], (512,512))
-
-##     glEnable(GL_TEXTURE_2D)
-##     glColor3f(1,1,1)
-
-##     for octave in octaves:
-##         octave.bind()
-
-##         glBegin(GL_QUADS)
-##         glTexCoord2f(0,0)
-##         glVertex2f(0,0)
-##         glTexCoord2f(2,0)
-##         glVertex2f(viewport.size[0],0)
-##         glTexCoord2f(2,2)
-##         glVertex2f(*viewport.size)
-##         glTexCoord2f(0,2)
-##         glVertex2f(0,viewport.size[1])
-##         glEnd()
-
-##         glEnable(GL_BLEND)
-##         glColor4f(1,1,1,1-persistence)
-
-## def keyDown(event):
-##     global persistence, r, seed
-##     if event.unicode == '[':
-##         persistence -= 0.05
-##     elif event.unicode == ']':
-##         persistence += 0.05
-##     print "Persistence: %s" % persistence
-## viewport.onKeyDown.observe(keyDown)
+def keyDown(event):
+    global persistence
+    if event.unicode == '[':
+        persistence -= 0.05
+    elif event.unicode == ']':
+        persistence += 0.05
+    print "Persistence: %s" % persistence
+viewport.onKeyDown.observe(keyDown)
 
 Util.showFrameRate(viewport)
 viewport.onDrawFrame.observe(drawFrame)
