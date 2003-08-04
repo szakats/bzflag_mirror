fb2059750394b3e777a23ab867398183060fbbba
davidtrowbridge <davidtrowbridge@08b3d480-bf2c-0410-a26f-811ee3361c24>
2003-08-04 22:59:58 +0000

    implement more of the test
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@2920 08b3d480-bf2c-0410-a26f-811ee3361c24

--- pybzflag/cgtest.py
+++ pybzflag/cgtest.py
@@ -3,20 +3,94 @@
 # Pretty much a straight translation of the CgGL example distributed with the Cg toolkit
 # this is -not- platform agnostic at the moment
 #
-import OpenGL.GL, sys
+import sys
 from BZFlag import Event
 from BZFlag.UI import Viewport, HUD, Layout
 from Cg import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+normals = [(-1.0, 0.0,  0.0),
+           (0.0,  1.0,  0.0),
+	   (1.0,  0.0,  0.0),
+	   (0.0, -1.0,  0.0),
+	   (0.0,  0.0,  1.0),
+	   (0.0,  0.0, -1.0)]
+
+faces = [(0, 1, 2, 3),
+         (3, 2, 6, 7),
+	 (7, 6, 5, 4),
+	 (4, 5, 1, 0),
+	 (5, 6, 2, 1),
+	 (7, 4, 0, 3)]
+
+vertices = [(-1, -1,  1),
+            (-1, -1, -1),
+	    (-1,  1, -1),
+	    (-1,  1,  1),
+	    ( 1, -1,  1),
+	    ( 1, -1, -1),
+	    ( 1,  1, -1),
+	    ( 1,  1,  1)]
+
+program = None
+Kd = None
+modelViewProj = None
+testColor = None
 
 def CheckCGError():
     error = Cg.cgGetError()
     if error != CG_NO_ERROR:
-	sys.exit('Cg error: %s' % Cg.cgGetErrorString(error))
+        sys.exit('Cg error: %s' % Cg.cgGetErrorString(error))
+
+def InitGL():
+    glViewport(0, 0, 800, 600)
+
+    glDisable(GL_LIGHTING)
+
+    glEnable(GL_DEPTH_TEST)
+    glMatrixMode(GL_PROJECTION)
+    gluPerspective(40, 1, 1, 10)
+    glMatrixMode(GL_MODELVIEW)
+    gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0)
+    glTranslatef(0, 0, -1)
+    glRotatef(60, 1, 0, 0)
+    glRotatef(-20, 0, 0, 1)
+
+def DrawCube():
+#    cgGLBindProgram(program)
+    CheckCGError()
+
+    if Kd != None:
+        cgGLSetParameter4f(Kd, 1.0, 1.0, 0.0, 1.0)
+
+    if modelViewProj != None:
+        cgGLSetStateMatrixParameter(modelViewProj, CG_GL_MODELVIEW_PROJECTION_MATRIX, CG_GL_MATRIX_IDENTITY)
+
+#    cgGLEnableProfile(CG_PROFILE_VP20)
+    CheckCGError()
+    glColor3f(1, 1, 1)
+
+    for i in range(5):
+        glBegin(GL_QUADS)
+	glNormal3fv(normals[i])
+	cgGLSetParameter3f(testColor, 1.0, 0.0, 0.0);
+	glVertex3fv(vertices[faces[i][0]])
+	cgGLSetParameter3f(testColor, 0.0, 1.0, 0.0);
+	glVertex3fv(vertices[faces[i][1]])
+	cgGLSetParameter3f(testColor, 0.0, 0.0, 1.0);
+	glVertex3fv(vertices[faces[i][2]])
+	cgGLSetParameter3f(testColor, 1.0, 1.0, 1.0);
+	glVertex3fv(vertices[faces[i][3]])
+	glEnd()
+
+    cgGLDisableProfile(CG_PROFILE_VP20)
+    CheckCGError()
 
 loop = Event.EventLoop()
 viewport = Viewport.OpenGLViewport(loop, (800, 600))
 
-context = Cg.cgCreateContext()
+context = cgCreateContext()
 CheckCGError()
 
 program = cgCreateProgramFromFile(context, CG_SOURCE, 'extra_media/cgGL_vertex_example.cg', CG_PROFILE_VP20, 'main', None)
@@ -40,8 +114,9 @@ CheckCGError()
 testColor = cgGetNamedParameter(program, 'IN.TestColor')
 CheckCGError()
 
-
+InitGL()
 HUD.Text(viewport.region(Layout.Rect(viewport).margin(10)), programString, fontSize=10)
+DrawCube()
 
 loop.run()
 
