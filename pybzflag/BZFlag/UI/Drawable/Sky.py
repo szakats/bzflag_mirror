bc735c98e298a2e8a02f92f9f550baa0ce96bbbc
micahjd <micahjd@08b3d480-bf2c-0410-a26f-811ee3361c24>
2003-08-04 11:19:33 +0000

    Oops, disable texturing and fix the widget colors
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@2912 08b3d480-bf2c-0410-a26f-811ee3361c24

--- pybzflag/BZFlag/UI/Drawable/Sky.py
+++ pybzflag/BZFlag/UI/Drawable/Sky.py
@@ -116,6 +116,7 @@ class Clouds(SkyDrawable):
         glDisable(GL_BLEND)
         glPopMatrix()
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        glDisable(GL_TEXTURE_2D)
 
     def draw(self, rstate):
         """Animate the texture coordinates every frame"""
