ca59bb39f00c3865f9fec9d463a7141ec548391e
micahjd <micahjd@08b3d480-bf2c-0410-a26f-811ee3361c24>
2003-08-04 11:15:02 +0000

    Wow, a use for the 'widget'
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@2911 08b3d480-bf2c-0410-a26f-811ee3361c24

--- pybzflag/skytest.py
+++ pybzflag/skytest.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python
-from BZFlag.UI import Viewport, ThreeDRender, ThreeDControl, Environment
+from BZFlag.UI import Viewport, ThreeDRender, ThreeDControl, Environment, Drawable
 from BZFlag import Event, Util
 
+# Viewport, view, and event loop setup
 loop = Event.EventLoop()
 viewport = Viewport.OpenGLViewport(loop, (800,600), 1000)
 view = ThreeDRender.View(viewport)
 control = ThreeDControl.Viewing(view, viewport)
 viewport.setCaption("Sky Test")
 
-sky = Environment.Sky()
+# Move the camera
+view.camera.position = (0,0,0)
+view.camera.distance = 400
+view.camera.jump()
 
+# Add the sky
+sky = Environment.Sky()
 view.scene.add(sky)
 viewport.onSetupFrame.observe(sky.update)
+
+# Add a navigation widget so we don't get too disoriented
+class Axes:
+    def __init__(self):
+        self.drawables = Drawable.VRML.load("nav_widget.wrl").values()
+    def getDrawables(self):
+        return self.drawables
+view.scene.add(Axes())
 
 view.scene.preprocess()
 Util.showFrameRate(viewport)
