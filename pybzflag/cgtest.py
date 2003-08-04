4a61c0fb01aeed649ae8531f18d000a0b9f39acc
davidtrowbridge <davidtrowbridge@08b3d480-bf2c-0410-a26f-811ee3361c24>
2003-08-04 05:24:13 +0000

    busted even more
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@2876 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ pybzflag/cgtest.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#
+# Pretty much a straight translation of the CgGL example distributed with the Cg toolkit
+# this is -not- platform agnostic at the moment
+#
+import pygame, OpenGL.GL, sys
+from Cg import *
+
+def CheckCGError():
+    error = Cg.cgGetError()
+    if error != CG_NO_ERROR:
+	sys.exit('Cg error: %s' % Cg.cgGetErrorString(error))
+
+context = Cg.cgCreateContext()
+CheckCGError()
+
+program = cgCreateProgramFromFile(context, CG_SOURCE, 'extra_media/cgGL_vertex_example.cg', CG_PROFILE_VP20, 'main', None)
+CheckCGError()
+
+print 'LAST LISTING----%s----' % cgGetLastListing(context)
+print '---- PROGRAM BEGIN ----'
+print cgGetProgramString(program, CG_COMPILED_PROGRAM)
+print '---- PROGRAM END ----'
