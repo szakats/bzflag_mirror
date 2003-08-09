779dd32397338246d09067c47bb216000c306e83
micahjd <micahjd@08b3d480-bf2c-0410-a26f-811ee3361c24>
2003-08-09 23:33:49 +0000

    Break everything
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@3057 08b3d480-bf2c-0410-a26f-811ee3361c24

--- pybzflag/BZFlag/UI/Drawable/Array.py
+++ pybzflag/BZFlag/UI/Drawable/Array.py
@@ -31,7 +31,109 @@ from BZFlag.UI import GLExtension
 __all__ = ['ArraySurface']
 
 
-class ArraySurface(GLDrawable):
+class VertexArray:
+    """An abstraction for using glInterleavedArrays to efficiently display geometry
+       stored into Numeric arrays for vertices, texture coordinates, and normals.
+       It doesn't specify how these arrays are rendered.
+       Subclasses of VertexArray and GLDrawable are provided to do this.
+       """
+    def __init__(self, shape, format):
+        # glInterleavedArrays in PyOpenGL leaks memory, this is a workaround
+        try:
+            from numeric_gl import glInterleavedArrays
+            self.glInterleavedArrays = glInterleavedArrays
+        except ImportError:
+            raise Exception("Due to a bug in PyOpenGL, you must install the numeric_gl module from " +
+                            "http://py3d.org/files/numeric_gl.tar.gz")
+
+        self.setupArrays(shape, format)
+
+    def setupArrays(self, shape, format):
+        """For each glInterleavedArrays format, we set up a properly sized array
+           for the raw interleaved data, then use Numeric's slice operator to assign
+           parts of it to named arrays.
+           """
+        self.shape     = shape
+        self.format    = format
+        self.vertices  = None
+        self.texcoords = None
+        self.normals   = None
+        self.colors    = None
+
+        if format == GL_V2F:
+            self.interleaved = zeros(shape + (2,), Float32)
+            self.vertices    = self.interleaved
+
+        elif format == GL_V3F:
+            self.interleaved = zeros(shape + (3,), Float32)
+            self.vertices    = self.interleaved
+
+        elif format == GL_C3F_V3F:
+            self.interleaved = zeros(shape + (6,), Float32)
+            self.colors      = self.interleaved[..., 0:3]
+            self.vertices    = self.interleaved[..., 3:6]
+
+        elif format == GL_N3F_V3F:
+            self.interleaved = zeros(shape + (6,), Float32)
+            self.normals     = self.interleaved[..., 0:3]
+            self.vertices    = self.interleaved[..., 3:6]
+
+        elif format == GL_C4F_N3F_V3F:
+            self.interleaved = zeros(shape + (10,), Float32)
+            self.colors      = self.interleaved[..., 0:4]
+            self.normals     = self.interleaved[..., 4:7]
+            self.vertices    = self.interleaved[..., 7:10]
+
+        elif format == GL_T2F_V3F:
+            self.interleaved = zeros(shape + (5,), Float32)
+            self.texcoords   = self.interleaved[..., 0:2]
+            self.vertices    = self.interleaved[..., 2:5]
+
+        elif format == GL_T4F_V4F:
+            self.interleaved = zeros(shape + (5,), Float32)
+            self.texcoords   = self.interleaved[..., 0:4]
+            self.vertices    = self.interleaved[..., 4:8]
+
+        elif format == GL_T2F_C3F_V3F:
+            self.interleaved = zeros(shape + (8,), Float32)
+            self.texcoords   = self.interleaved[..., 0:2]
+            self.colors      = self.interleaved[..., 2:5]
+            self.vertices    = self.interleaved[..., 5:8]
+
+        elif format == GL_T2F_N3F_V3F:
+            self.interleaved = zeros(shape + (8,), Float32)
+            self.texcoords   = self.interleaved[..., 0:2]
+            self.normals     = self.interleaved[..., 2:5]
+            self.vertices    = self.interleaved[..., 5:8]
+
+        elif format == GL_T2F_C4F_N3F_V3F:
+            self.interleaved = zeros(shape + (12,), Float32)
+            self.texcoords   = self.interleaved[..., 0:2]
+            self.colors      = self.interleaved[..., 2:6]
+            self.normals     = self.interleaved[..., 6:9]
+            self.vertices    = self.interleaved[..., 9:12]
+
+        elif format == GL_T4F_C4F_N3F_V4F:
+            self.interleaved = zeros(shape + (15,), Float32)
+            self.texcoords   = self.interleaved[..., 0:4]
+            self.colors      = self.interleaved[..., 4:8]
+            self.normals     = self.interleaved[..., 8:11]
+            self.vertices    = self.interleaved[..., 11:15]
+
+        else:
+            raise Exception("Unsupported glInterleavedArrays format")
+
+    def bind(self):
+        """Make this vertex array active in the current OpenGL context.
+           After this call, the vertex array can be drawn from using glDrawElements()
+           or glDrawArrays()
+           """
+        if GLExtension.multitexture:
+            glClientActiveTextureARB(GL_TEXTURE0_ARB)
+        self.glInterleavedArrays(self.format, 0, self.interleaved)
+
+
+class ArraySurface(GLDrawable, VertexArray):
     """Drawable representing a smooth surface composed of a grid of vertices,
        contained in a Numeric array. The array may change between renderings,
        since normals are recalculated each time draw() is called. The size of
@@ -42,8 +144,9 @@ class ArraySurface(GLDrawable):
        cause normals to be recalculated at each rendering, so it will then be
        suitable for dynamic surfaces.
        """
-    def __init__(self, vertices, texcoords=None):
+    def __init__(self):
         GLDrawable.__init__(self)
+        VertexArray.__init__(self)
         self.vertices = vertices
         self.texcoords = texcoords
         if self.texcoords is None:
@@ -62,14 +165,6 @@ class ArraySurface(GLDrawable):
         self.gridNormals = zeros(self.crossProducts.shape, self.vertices.typecode())
         self.normals = zeros(self.vertices.shape, self.vertices.typecode())
         self.interleaved = zeros(self.vertices.shape[:-1] + (8,), self.vertices.typecode())
-
-        # glInterleavedArrays in PyOpenGL leaks memory, this is a workaround
-        try:
-            from numeric_gl import glInterleavedArrays
-            self.glInterleavedArrays = glInterleavedArrays
-        except ImportError:
-            raise Exception("Due to a bug in PyOpenGL, you must install the numeric_gl module from " +
-                            "http://py3d.org/files/numeric_gl.tar.gz")
 
     def prepareIndices(self):
         """Prepare an array with indices into the vertex array for triangle stripss.
