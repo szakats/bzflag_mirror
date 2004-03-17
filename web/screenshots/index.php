f3020a345e8e4b9349c38d7b61c8f4ca9831120c
timriker <timriker@08b3d480-bf2c-0410-a26f-811ee3361c24>
2004-03-17 06:08:42 +0000

    new images
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@6376 08b3d480-bf2c-0410-a26f-811ee3361c24

--- web/screenshots/index.php
+++ web/screenshots/index.php
@@ -4,8 +4,35 @@ include "../document.php";
 $doc = new Document;
 $doc->begin("screenshots");
 print <<< end
-These are some screenshots of the up-and-coming 1.8 version. Soon we'll get
-some shots from the current stable version, 1.7e6.<br><br><br>
+<h1>Screenshots</h1>
+<p>Feel free to <a href="mailto:learner AT brlcad DOT org">submit a screenshot</a> that you would like to see added.  Screenshots can be created while playing BZFlag by pressing the F5 key.  This will generate either a raw image (sgi format) or a gamma-corrected png, depending on your version of BZFlag.</p>
+
+<p>Fullsize mages should be converted to either jpeg or png format.  Any resolution is okay, but please include a jpeg thumbnail if you would.  Thumbnail images should be approximately no more than 128 pixels wide, preserving the original aspect ratio.  <tiny>(*nix users can try running "<code>djpeg image.jpg  | pnmscale -pixels 16384 | cjpeg -progressive > t-image.jpg</code>")</tiny></p>
+
+<br>
+
+<table border=0 cellspacing=16 ><tr><td align="center">
+
+<table border=0><tr><td align="center">
+<a href="c3po.jpg"><img src="t-c3po.jpg" border=0 alt="he didn't even see him coming"></a>
+</td></tr><tr><td align="center">
+c3po destroys a guided missle shooter with grace and style
+</td></tr></table>
+
+</td></tr><tr><td align="center">
+
+<table border=0><tr><td align="center">
+<a href="bzadminphone.jpg"><img src="t-bzadminphone.jpg" border=0 alt="now if we could just play bzflag on it too..."></a>
+</td></tr><tr><td align="center">
+Nidhoggr shows bzadmin running on his phone
+</td></tr></table>
+
+</td></tr></table>
+
+<br><br>
+<hr>
+<p>These are some screenshots of the experimental 1.8 version.  Much of the effort for this release was included in the 1.10 version.</p>
+<br>
 <a href="bzfi0000.jpg"><img src="t-bzfi0000.jpg" border="0" alt="screenshot 0000"></a>
 <a href="bzfi0001.jpg"><img src="t-bzfi0001.jpg" border="0" alt="screenshot 0001"></a>
 <a href="bzfi0002.jpg"><img src="t-bzfi0002.jpg" border="0" alt="screenshot 0002"></a>
@@ -37,6 +64,37 @@ some shots from the current stable version, 1.7e6.<br><br><br>
 <a href="bzfi0028.jpg"><img src="t-bzfi0028.jpg" border="0" alt="screenshot 0028"></a>
 <a href="bzfi0029.jpg"><img src="t-bzfi0029.jpg" border="0" alt="screenshot 0029"></a>
 <a href="bzfi0030.jpg"><img src="t-bzfi0030.jpg" border="0" alt="screenshot 0030"></a>
+
+<hr>
+<h1>Other Images</h1>
+
+<p>Here are some fun images to look at.  Feel free to <a href="mailto:learne AT brlcad DOT org">submit</a> other BZFlag-related artwork.</p>
+<br>
+
+<table border=0 cellspacing=16 ><tr><td align="center">
+
+<table border=0><tr><td align="center">
+<a href="bzkitty.jpg"><img src="t-bzkitty.jpg" border=0 alt="bring it on..."></a>
+</td></tr><tr><td align="center">
+daBomb's kitty is ready for battle
+</td></tr></table>
+
+</td></tr></table>
+
+<!--
+
+<hr>
+<h1>Other Images</h1>
+
+<p>Here are some fun half-random images to look at.  Feel free to <a href="mailto:learne AT brlcad DOT org">submit</a> other BZFlag-related artwork.</p>
+<br>
+<table border=0><tr><td align="center">
+<img src="" border=0 alt="">
+</td></tr><tr><td align="center">
+picture comment goes here
+</td></tr></table>
+-->
+
 end;
 $doc->end();
 
