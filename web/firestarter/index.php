9f78fd49551aec27d0b5bd229914bd1834a2256e
jeffm2501 <jeffm2501@08b3d480-bf2c-0410-a26f-811ee3361c24>
2004-05-28 19:14:23 +0000

    add fs redir to cvs
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@6943 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ web/firestarter/index.php
@@ -0,0 +1,11 @@
+<?php header ('Location: http://firestarter.bakadigital.com/');
+ ?>
+ 
+<html>
+<head>
+<title>Untitled Document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+ <body>
+</body>
+</html>
