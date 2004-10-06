828820d298fd382f26c5325f06fbf1e93247bd56
brlcad <brlcad@08b3d480-bf2c-0410-a26f-811ee3361c24>
2004-10-06 06:23:49 +0000

    update MOTD to support a bunch of new fields including title, author, description, version, publication date, and a mostly unique ID (assumes only 1 message being posted per sec).
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@8482 08b3d480-bf2c-0410-a26f-811ee3361c24

--- web/admin/setMOTD.php
+++ web/admin/setMOTD.php
@@ -20,10 +20,10 @@ if(isset($sessionID)) {
   if(mysql_num_rows($result) == 1 && $substr[2] >= 1) {
     if(!isset($add)) {
 	// check for the table
-	 if( !table_exist('motd')){	
-	 	 $query = "CREATE TABLE motd ( message varchar(255) NOT NULL default '') TYPE=MyISAM COMMENT='the current message';";
+	 if( !table_exist('motd')){
+	 	 $query = "CREATE TABLE motd ( title varchar(80) NOT NULL default '', description varchar(255) NOT NULL default '', author varchar(64) NOT NULL default '', pubDate varchar(64) NOT NULL default '', version varchar(64) NOT NULL default '', id bigint(20) unsigned NOT NULL default 0 ) TYPE=MyISAM COMMENT='the current message';";
 		 $result = mysql_query($query);
-		 $query = "INSERT INTO motd (message) values ('Welcome to BZFlag')";
+		 $query = "INSERT INTO motd (title, description, author, pubDate, version, id) values ('Welcome to BZFlag', 'BZFlag is a free multiplayer multiplatform 3D tank battle game. The name stands for Battle Zone capture Flag. It runs on Irix, Linux, BSD, Windows, Mac OS X and other platforms.', 'BZFlag Developers', '', '0.0', '0' )";
 		 $result = mysql_query($query);
 	 }
 	$query = "SELECT * FROM motd";
@@ -35,9 +35,18 @@ print <<< end
 CurrentMOTD: $currentMOTD</br>
 <form method="post" action="setMOTD.php">
 <input type="hidden" name="add" value="1">
-<b>Message:</b><br>
-<textarea rows="8" cols="45" name="body"></textarea><br>
-<input type="submit" value="Set MOTD"><br>
+<table cellspacing=10 cellpadding=0 border=0>
+  <tr><td align="right">Author:</td><td align="left"><input type="text" size=40 maxlength=64 name="author" value="BZFlag Developers"></td></tr>
+  <tr><td align="right">Publication Date:</td><td align="left">
+end;
+ echo "<input type=\"text\" size=40 maxlength=64 name=\"pubDate\" value=\"" . gmdate('D, d M Y H:i:s T') . "\"></td></tr>\n";
+
+print <<< end
+  <tr><td align="right">Version:</td><td align="left"><input type="text" size=40 maxlength=64 name="version" value="1.12"></td></tr>
+  <tr><td align="right">Title:</td><td align="left"><input type="text" size=80 maxlength=80 name="title"></td></tr>
+  <tr><td align="right">Message:</td><td align="left"><textarea rows=8 cols=40 name="description"></textarea></td></tr>
+  <tr><td align="right"><input type="submit" value="Set MOTD"></td><td align="left">&nbsp;</tr>
+</table>
 </form>
 end;
     } else {
@@ -46,11 +55,20 @@ print <<< end
 end;
       $substr = explode("|", $sessionID);
       $user = $substr[0];
-      $query = "UPDATE motd SET message='$body';";
+      $id = gmdate('YmdHis');
+      $query = "INSERT INTO motd (title, description, author, pubDate, version, id) values ('$title', '$description', '$author', '$pubDate', '$version', $id );";
       $result = mysql_query($query);
-      $result = mysql_query("SELECT * FROM motd");
-      $currow = mysql_fetch_array($result);
-      printf("New MOTD:<b>%s</b><br><br>\n", $currow["message"]);
+      if ($result) {
+	$result = mysql_query("SELECT * FROM motd WHERE id='$id'");
+	if ($result) {
+	  $currow = mysql_fetch_array($result);
+	  printf("New MOTD:<b>%s</b><br>Posted on %s by %s for BZFlag %s<br>%s<br><br>\n", $currow["title"], $currow["pubDate"], $currow["author"], $currow["version"], $currow["description"] );
+	} else {
+	  printf("Unable to get the latest MOTD<br>");
+	}
+      } else {
+	printf("Unable to insert into the database<br><br>Query:<br><code>%s</code>\n<br>", $query);
+      }
     }
   } else {
 print <<< end
