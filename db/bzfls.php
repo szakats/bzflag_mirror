e638444af64bddc44c47b7c235165d4f72b65114
timriker <timriker@08b3d480-bf2c-0410-a26f-811ee3361c24>
2004-11-09 15:33:30 +0000

    reorg
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@8839 08b3d480-bf2c-0410-a26f-811ee3361c24

--- db/bzfls.php
+++ db/bzfls.php
@@ -25,30 +25,30 @@ include('serversettings.php');
 // $dbpass  = "bzflag";
 
 # Common to all
-$action   = $_GET['action'];
-if (!array_key_exists("action", $_GET)) {
+$action   = $_REQUEST['action'];
+if (!array_key_exists("action", $_REQUEST)) {
   $action = "UNKNOWN";
 }
 
 # For bzfs
-$nameport = $_GET['nameport'];
+$nameport = $_REQUEST['nameport'];
 
 # For ADD
-$build    = $_GET['build'];
-$version  = $_GET['version']; # also on LIST
-$gameinfo = $_GET['gameinfo'];
-$title    = $_GET['title'];
-$local    = $_GET['local'];
+$build    = $_REQUEST['build'];
+$version  = $_REQUEST['version']; # also on LIST
+$gameinfo = $_REQUEST['gameinfo'];
+$title    = $_REQUEST['title'];
+$local    = $_REQUEST['local'];
 
 # For players
-$callsign = $_GET['callsign'];  # urlencoded
-$email    = $_GET['email'];     # urlencoded
-$password = $_GET['password'];  # urlencoded
-$conntime = $_GET['conntime'];  # TODO kill this. use db server time for everything
+$callsign = $_REQUEST['callsign'];  # urlencoded
+$email    = $_REQUEST['email'];     # urlencoded
+$password = $_REQUEST['password'];  # urlencoded
+$conntime = $_REQUEST['conntime'];  # TODO kill this. use db server time for everything
 
 # for CHECKTOKEN
-$playerid = $_GET['playerid'];
-$token = $_GET['token'];
+$playerid = $_REQUEST['playerid'];
+$token = $_REQUEST['token'];
 
 # for banning.  provide key => value pairs where the key is an
 # ip address. value is not used at present.
@@ -64,8 +64,9 @@ $alternateServers = array("");
 
 # log function
 if ($enableDebug) {
-  function debug ($filename, $message) {
-    $fp = fopen($filename, "a");
+  function debug ($message) {
+    global $debugFile;
+    $fp = fopen($debugFile, "a");
     if ($fp) {
       # output the message with a BSD-style timestamp
       fwrite($fp, date("D M j G:i:s T Y") . " " . $_SERVER['REMOTE_ADDR'] . " " . $message . "\n");
@@ -74,55 +75,72 @@ if ($enableDebug) {
       print("Unable to write to to log file [$filename]");
     }
   }
-
 } else {
-  function debug ($filename, $message) {
+  function debug ($message) {
   }
 }
 
-# ignore banned servers outright
-if ($banlist[$_SERVER['REMOTE_ADDR']] != "") {
-  # reject the connection attempt
-  header("Content-type: text/plain");
-  $remote_addr = $_SERVER['REMOTE_ADDR'];
-  debug($debugFile, "Connection rejected from $remote_addr");
-  die("Connection attempt rejected.  See #bzflag on irc.freenode.net");
+function testform ($message) {
+  global $action;
+  header("Content-type: text/html");
+  print('<html>
+<head>
+<title>BZFlag db server</title>
+</head>
+<body>
+  <h1>BZFlag db server</h1>
+  ' . $message . '
+  <p>This is the development interface to the <a href="http://BZFlag.org/">BZFlag</a> list server.</p>
+  <form action="" method="GET">
+    action:<select name="action">
+	<option value="LIST" selected>LIST - list servers</option>
+	<option value="DEBUG">DEBUG - developer interface</option>
+	<option value="ADD">ADD - add a server</option>
+	<option value="REMOVE">REMOVE - remove a server</option>
+	<option value="REGISTER">REGISTER - new player</option>
+	<option value="CONFIRM">CONFIRM - confirm registration</option>
+	<option value="QUIT">QUIT - FIXME</option>
+	<option value="CHECKTOKEN">CHECKTOKEN - verify player token from game server</option>
+	<option value="UNKNOWN">UNKNOWN - test invalid request</option>
+    </select><br>
+    actions: LIST<br>
+    version:<input type="text" name="version" size="80"><br>
+    local:<input type="text" name="local" size="5" value="1"><br>
+    callsign:<input type="text" name="callsign" size="80"><br>
+    password:<input type="text" name="password" size="80"><br>
+    actions: ADD REMOVE<br>
+    nameport:<input type="text" name="nameport" size="80"><br>
+    build:<input type="text" name="build" size="80"><br>
+    gameinfo:<input type="text" name="gameinfo" size="80"><br>
+    title:<input type="text" name="title" size="80"><br>
+    actions: REGISTER CONFIRM QUIT<br>
+    email:<input type="text" name="email" size="80"><br>
+    actions: CHECKTOKEN<br>
+    playerid:<input type="text" name="playerid" size="80"><br>
+    token:<input type="text" name="token" size="80"><br>
+    <input type="submit" value="Post entry">
+    <input type="reset" value="Clear form">
+  </form>
+</body>');
+  if ($action == "DEBUG") {
+    print('<PRE>\n');
+    var_dump($GLOBALS);
+    print('</PRE>\n');
+    phpinfo();
+  }
+  print('</body>
+</html>');
 }
 
-debug($debugFile, "Connecting to the database");
-
-# Connect to the server database persistently.
-$link = mysql_pconnect($dbhost, $dbuname, $dbpass)
-     or die("Could not connect: " . mysql_error());
-if (!mysql_select_db($dbname)) {
-  debug($debugFile, "Database did not exist");
-
-  die("Could not open db: " . mysql_error());
-}
-
-# remove all inactive registered players from the table
-# FIXME this should not happen on every request
-$timeout = 31536000; # timeout in seconds, 365 days
-$staletime = time() - $timeout;
-mysql_query("DELETE FROM players WHERE lastmod < $staletime", $link)
-  or die ("Could not remove inactive players" . mysql_error());
-
-# remove all players who have not confirmed registration
-# FIXME this should not happen on every request
-$timeout = 259200;  # timeout in seconds, 72h
-$staletime = time() - $timeout;
-mysql_query("DELETE FROM players WHERE lastmod < $staletime AND randtext != NULL", $link)
-  or die ("Could not remove inactive players" . mysql_error());
-
-# Do stuff based on what the 'action' is...
-if ($action == "LIST" ) {
+function action_list () {
   #  -- LIST --
   # Same as LIST in the old bzfls
+  global $link, $callsign, $password, $version, $local;
   header("Content-type: text/plain");
-  debug($debugFile, "Fetching LIST");
+  debug("Fetching LIST");
 
   # remove all inactive servers from the table
-  debug($debugFile, "Deleting inactive servers from list");
+  debug("Deleting inactive servers from list");
   $timeout = 1800;    # timeout in seconds
   $staletime = time() - $timeout;
   mysql_query("DELETE FROM servers WHERE lastmod < $staletime", $link)
@@ -175,12 +193,15 @@ if ($action == "LIST" ) {
 	readfile($thisSever.'?action=LIST&local=1');
     }
   }
-} elseif ($action == "ADD") {
+}
+
+function action_add () {
   #  -- ADD --
   # Server either requests to be added to DB, or to issue a keep-alive so that it
   # does not get dropped due to a timeout...
+  global $link, $nameport, $version, $gameinfo, $title;
   header("Content-type: text/plain");
-  debug($debugFile, "Attempting to ADD $nameport $version $gameinfo $title");
+  debug("Attempting to ADD $nameport $version $gameinfo $title");
 
   # Filter out badly formatted or buggy versions
   print "trying ADD $nameport $version $gameinfo $title\n";
@@ -196,13 +217,13 @@ if ($action == "LIST" ) {
   $servip = gethostbyname($servname);
 
   if ($servip == "0.0.0.0") {
-    debug($debugFile, "Changed " . $servip . " to requesting address: "
+    debug("Changed " . $servip . " to requesting address: "
 	. $_SERVER['REMOTE_ADDR'] );
     $servip =  $_SERVER['REMOTE_ADDR'];
     $servname = $servip;
     $nameport = $servip . ":" . $servport;
   }elseif ($_SERVER['REMOTE_ADDR'] != $servip) {
-    debug($debugFile, "Requesting address is " . $_SERVER['REMOTE_ADDR']
+    debug("Requesting address is " . $_SERVER['REMOTE_ADDR']
 	. " while server is at " . $servip );
     print("Requesting address is " . $_SERVER['REMOTE_ADDR']
 	. " while server is at " . $servip );
@@ -228,7 +249,7 @@ if ($action == "LIST" ) {
     or die ("Invalid query: ". mysql_error());
   $count = mysql_num_rows($result);
   if (!$count) {
-    debug($debugFile, "Server does not already exist in database -- adding");
+    debug("Server does not already exist in database -- adding");
 
     # Server does not already exist in DB so insert into DB
     $result = mysql_query("INSERT INTO servers "
@@ -239,7 +260,7 @@ if ($action == "LIST" ) {
       or die ("Invalid query: ". mysql_error());
   } else {
 
-    debug($debugFile, "Server already exists in database -- updating");
+    debug("Server already exists in database -- updating");
 
     # Server exists already, so update the table entry
     # ASSUMPTION: only the 'lastmod' column of table needs updating since all
@@ -254,14 +275,17 @@ if ($action == "LIST" ) {
       or die ("Invalid query: ". mysql_error());
   }
 
-  debug($debugFile, "ADD complete");
+  debug("ADD complete");
 
   print "ADD complete\n";
-} elseif ($action == "REMOVE") {
+}
+
+function action_remove () {
   #  -- REMOVE --
   # Server requests to be removed from the DB.
+  global $link, $nameport;
   header("Content-type: text/plain");
-  debug($debugFile, "REMOVE request from $nameport");
+  debug("REMOVE request from $nameport");
 
   $split = explode(":", $nameport);
   $servname = $split[0];
@@ -271,13 +295,13 @@ if ($action == "LIST" ) {
     $servport = 5154;
   $servip = gethostbyname($servname);
   if ($servip == "0.0.0.0") {
-    debug($debugFile, "Changed " . $servip . " to requesting address: "
+    debug("Changed " . $servip . " to requesting address: "
 	. $_SERVER['REMOTE_ADDR'] );
     $servip =  $_SERVER['REMOTE_ADDR'];
     $servname = $servip;
     $nameport = $servip . ":" . $servport;
   } elseif ($_SERVER['REMOTE_ADDR'] != $servip) {
-    debug($debugFile, "Requesting address is " . $_SERVER['REMOTE_ADDR']
+    debug("Requesting address is " . $_SERVER['REMOTE_ADDR']
 	. " while server is at " . $servip );
     print("Requesting address is " . $_SERVER['REMOTE_ADDR']
 	. " while server is at " . $servip );
@@ -286,9 +310,12 @@ if ($action == "LIST" ) {
 
   $result = mysql_query("DELETE FROM servers WHERE nameport = '$nameport'", $link)
     or die ("Invalid query: ". mysql_error());
-} elseif ($action == "REGISTER") {
+}
+
+function action_register () {
   #  -- REGISTER --
   # Registers a player onto the players database.
+  global $link, $callsign, $email, $password;
   # see if there is an existing entry
   header("Content-type: text/plain");
   $result = mysql_query("SELECT * FROM players WHERE email = '$email'", $link)
@@ -331,9 +358,12 @@ if ($action == "LIST" ) {
   print("You will receive an email informing you on how to complete your account registration\n");
   print("While we are debugging, the link is posted here as well.:\n" .
        "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['SCRIPT_NAME'] . "?action=CONFIRM&email=$email&password=$randtext\n");
-} elseif ($action == "CONFIRM") {
+}
+
+function action_confirm () {
   #  -- CONFIRM --
   # Confirms a registration
+  global $link, $email, $password;
   header("Content-type: text/html");
   print("<html><head><title>Confirm registration</title></head><body>\n");
   $result = mysql_query("SELECT randtext FROM players WHERE email='$email'", $link)
@@ -355,9 +385,12 @@ if ($action == "LIST" ) {
     }
   }
   print('See <a href="http://BZFlag.org">http://BZFlag.org</a></body></html>');
-} elseif ($action == "CHECKTOKEN") {
+}
+
+function action_checktoken () {
   #  -- CHECKTOKEN --
   # validate player token for connecting player on a game server
+  global $link, $playerid, $token;
   # TODO add grouplist support
   header("Content-type: text/plain");
   $timeout = 600; # 10 minutes while testing
@@ -369,82 +402,106 @@ if ($action == "LIST" ) {
     or die ("Invalid query: " . mysql_error());
   $row = mysql_fetch_row($result);
   $callsign = $row[0];
-  # clear tokendate so nasty game server admins can't login someplace else
-  $result = mysql_query("UPDATE players SET "
-      . "lastmod = '" . time() . "', "
-      . "tokendate = '0' "
-      . "WHERE playerid='$playerid'", $link)
-    or die ("Invalid query: " . mysql_error());
-  if ($callsign)
+  if ($callsign) {
+    # clear tokendate so nasty game server admins can't login someplace else
+    $result = mysql_query("UPDATE players SET "
+	. "lastmod = '" . time() . "', "
+	. "tokendate = '0' "
+	. "WHERE playerid='$playerid'", $link)
+      or die ("Invalid query: " . mysql_error());
     print ("$callsign:\n");
-  else
+  } else
     print ("FAIL");
-} elseif ($action == "QUIT") {
+}
+
+function action_quit () {
   #  -- QUIT --
   # handle disconnecting player
+  # TODO not actually used yet
+  global $link, $callsign;
   $result = mysql_query("UPDATE players SET lastmod=" . time()
       . ", playtime=playtime+$conntime WHERE callsign='$callsign'", $link)
     or die ("Invalid query: " . mysql_error());
-} else {
-  header("Content-type: text/html");
+}
+
+# ignore banned servers outright
+if ($banlist[$_SERVER['REMOTE_ADDR']] != "") {
+  # reject the connection attempt
+  header("Content-type: text/plain");
+  $remote_addr = $_SERVER['REMOTE_ADDR'];
+  debug("Connection rejected from $remote_addr");
+  die("Connection attempt rejected.  See #bzflag on irc.freenode.net");
+}
+
+debug("Connecting to the database");
+
+# Connect to the server database persistently.
+$link = mysql_pconnect($dbhost, $dbuname, $dbpass)
+     or die("Could not connect: " . mysql_error());
+if (!mysql_select_db($dbname)) {
+  debug("Database did not exist");
+
+  die("Could not open db: " . mysql_error());
+}
+
+# remove all inactive registered players from the table
+# FIXME this should not happen on every request
+$timeout = 31536000; # timeout in seconds, 365 days
+$staletime = time() - $timeout;
+mysql_query("DELETE FROM players WHERE lastmod < $staletime", $link)
+  or die ("Could not remove inactive players" . mysql_error());
+
+# remove all players who have not confirmed registration
+# FIXME this should not happen on every request
+$timeout = 259200;  # timeout in seconds, 72h
+$staletime = time() - $timeout;
+mysql_query("DELETE FROM players WHERE lastmod < $staletime AND randtext != NULL", $link)
+  or die ("Could not remove inactive players" . mysql_error());
+
+# Do stuff based on what the 'action' is...
+switch ($action) {
+case "LIST":
+  action_list();
+  break;
+case "ADD":
+  action_add();
+  break;
+case "REMOVE":
+  action_remove();
+  break;
+case "REGISTER":
+  action_register();
+  break;
+case "CONFIRM";
+  action_confirm();
+  break;
+case "CHECKTOKEN":
+  action_checktoken();
+  break;
+case "QUIT":
+  action_quit();
+  break;
+case "DEBUG":
+  testform('');
+  break;
+default:
   # TODO dump the default form here but still close the database connection
-  print('<html>
-<head>
-<title>BZFlag db server</title>
-</head>
-<body>
-  <h1>BZFlag db server</h1>
-  Unknown command: \'' . $action . '\'
-  <p>This is the development interface to the <a href="http://BZFlag.org/">BZFlag</a> list server.</p>
-  <form action="" method="get">
-    action:<select name="action">
-	<option value="LIST" selected>LIST - list servers</option>
-	<option value="ADD">ADD - add a server</option>
-	<option value="REMOVE">REMOVE - remove a server</option>
-	<option value="REGISTER">REGISTER - new player</option>
-	<option value="CONFIRM">CONFIRM - confirm registration</option>
-	<option value="QUIT">QUIT - FIXME</option>
-	<option value="CHECKTOKEN">CHECKTOKEN - verify player token from game server</option>
-	<option value="UNKNOWN">UNKNOWN - test invalid request</option>
-    </select><br>
-    actions: LIST<br>
-    version:<input type="text" name="version" size="80"><br>
-    local:<input type="text" name="local" size="5" value="1"><br>
-    actions: ADD REMOVE<br>
-    nameport:<input type="text" name="nameport" size="80"><br>
-    build:<input type="text" name="build" size="80"><br>
-    gameinfo:<input type="text" name="gameinfo" size="80"><br>
-    title:<input type="text" name="title" size="80"><br>
-    actions: REGISTER CONFIRM QUIT<br>
-    callsign:<input type="text" name="callsign" size="80"><br>
-    password:<input type="text" name="password" size="80"><br>
-    email:<input type="text" name="email" size="80"><br>
-    actions: CHECKTOKEN<br>
-    playerid:<input type="text" name="playerid" size="80"><br>
-    token:<input type="text" name="token" size="80"><br>
-    <input type="submit" value="Post entry">
-    <input type="reset" value="Clear form">
-  </form>
-</body>');
-  //var_dump($buffer);
-  //phpinfo();
-  die('</body>
-</html>');
+  testform('Unknown command: \'' . $action . '\'');
 }
 
 # make sure the connection to mysql is severed
 if ($link) {
   # for a transaction commit just in case
-  debug($debugFile, "Commiting any pending transactions");
+  debug("Commiting any pending transactions");
   mysql_query("COMMIT", $link);
 
-  # debug($debugFile, "Closing link to database");
+  # debug("Closing link to database");
 
   # say bye bye (shouldn't need to ever really, especially for persistent..)
   #mysql_close($link);
 }
 
-debug($debugFile, "End session");
+debug("End session");
 
 # Local Variables: ***
 # mode:php ***
