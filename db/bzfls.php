a353ad6ce4d4f3a5196596a302b383261206116c
timriker <timriker@08b3d480-bf2c-0410-a26f-811ee3361c24>
2004-11-13 06:40:11 +0000

    don't take the error as a token
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@8898 08b3d480-bf2c-0410-a26f-811ee3361c24

--- db/bzfls.php
+++ db/bzfls.php
@@ -160,7 +160,7 @@ function action_list () {
     $row = mysql_fetch_row($result);
     $playerid = $row[0];
     if (!$playerid) {
-      print("TOKEN:ERROR: invalid callsign or password ($callsign:$password)\n");
+      print("NOTOK invalid callsign or password ($callsign:$password)\n");
     } else {
       srand(microtime() * 100000000);
       $token = rand(0,2147483647);
