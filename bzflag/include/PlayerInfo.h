a8753bd6810d1b7b6d5f349d481913bc2051b57e
(no author) <(no author)@08b3d480-bf2c-0410-a26f-811ee3361c24>
2005-11-12 21:23:51 +0000

    This commit was manufactured by cvs2svn to create tag 'merge-2_0-2_1-4'.
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/tags/merge-2_0-2_1-4@12058 08b3d480-bf2c-0410-a26f-811ee3361c24

--- bzflag/include/PlayerInfo.h
+++ bzflag/include/PlayerInfo.h
@@ -59,7 +59,7 @@ class PlayerInfo {
 public:
   PlayerInfo(int _playerIndex);
 
-  int   getPlayerIndex( void ) {return playerIndex;}
+  int   getPlayerIndex( void ) const { return playerIndex; } 
   void	setLastMsg(std::string msg);
   const std::string& getLastMsg() const;
   TimeKeeper  getLastMsgTime() const;
@@ -87,7 +87,7 @@ public:
   void       *packVirtualFlagCapture(void *buf);
   bool	isTeam(TeamColor team) const;
   bool	isObserver() const;
-  TeamColor   getTeam();
+  TeamColor   getTeam() const;
   void	setTeam(TeamColor team);
   void	wasARabbit();
   void	wasNotARabbit();
@@ -98,6 +98,7 @@ public:
   void	setFlag(int flag);
   bool	isFlagTransitSafe();
   const char *getClientVersion();
+  void getClientVersionNumbers(int& major, int& minor, int& revision);
   std::string getIdleStat();
   bool	canBeRabbit(bool relaxing = false);
   void	setPaused(bool paused);
@@ -144,6 +145,9 @@ private:
   char email[EmailLen];
   // version information from client
   char clientVersion[VersionLen];
+  int clientVersionMajor;
+  int clientVersionMinor;
+  int clientVersionRevision;
 
   // player's team
   TeamColor team;
--- bzflag/src/common/cURLManager.cxx
+++ bzflag/src/common/cURLManager.cxx
@@ -86,7 +86,7 @@ void cURLManager::setup()
 {
   CURLcode result;
 
-  DEBUG1("LIBCURL:	 %s\n", curl_version());
+  DEBUG1("LIBCURL version:  %s\n", curl_version());
   if ((result = curl_global_init(CURL_GLOBAL_NOTHING)))
     DEBUG1("cURL Global init Error: %d\n", result);
   multiHandle = curl_multi_init();
@@ -108,9 +108,18 @@ void cURLManager::setTimeout(long timeout)
 {
   CURLcode result;
 
-  result = curl_easy_setopt(easyHandle, CURLOPT_TIMEOUT, timeout);
+  result = curl_easy_setopt(easyHandle, CURLOPT_CONNECTTIMEOUT, timeout);
   if (result != CURLE_OK) {
-    DEBUG1("CURLOPT_TIMEOUT error %d : %s\n", result, errorBuffer);
+    DEBUG1("CURLOPT_CONNECTTIMEOUT error %d : %s\n", result, errorBuffer);
+  }
+  // anything under 1Kbyte/sec is junk
+  result = curl_easy_setopt(easyHandle, CURLOPT_LOW_SPEED_LIMIT, 1024);
+  if (result != CURLE_OK) {
+    DEBUG1("CURLOPT_LOW_SPEED_LIMIT error %d : %s\n", result, errorBuffer);
+  }
+  result = curl_easy_setopt(easyHandle, CURLOPT_LOW_SPEED_TIME, timeout);
+  if (result != CURLE_OK) {
+    DEBUG1("CURLOPT_LOW_SPEED_TIME error %d : %s\n", result, errorBuffer);
   }
 }
 
