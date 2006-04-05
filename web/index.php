0092aa9c015e3fbf730b643cfe91e4a723e8ae7c
jeffm2501 <jeffm2501@08b3d480-bf2c-0410-a26f-811ee3361c24>
2006-04-05 17:43:46 +0000

    update with latest changes
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@12703 08b3d480-bf2c-0410-a26f-811ee3361c24

--- web/index.php
+++ web/index.php
@@ -7,12 +7,26 @@ $doc->begin("home");
 $result = mysql_query("SELECT * FROM news ORDER BY date DESC");
 
 print <<< end
+<table border="0">
+<tr>
+<td>
+	<div align="center"><b>What is BZFlag:</b></div><br>
+</td>
+</tr>
+<tr>
+<td>
 BZFlag is a free multiplayer multiplatform 3D tank battle game. The name stands
 for Battle Zone capture Flag. It runs on Irix, Linux, *BSD, Windows, Mac OS X
 and other platforms. It's one of the most popular games ever on Silicon
 Graphics machines. For more information, check out some of the <a
-href="reviews.php">reviews</a>.<br><br>
-
+href="reviews.php">reviews</a>.
+</td>
+</tr>
+</table>
+<br><br>
+<table bgcolor="#EEEEEE" border="0" class="sidebar_border">
+<tr>
+<td>
 <div align="center"><b>Latest News:</b></div><br>
 end;
 
@@ -22,12 +36,12 @@ if(mysql_num_rows($result) == 0) {
   $i = 0;
   while($currow = mysql_fetch_array($result)) {
     if($i < 5) {
-      printf("<b>%s</b> - %s by %s<br>%s<br><br>\n", $currow["headline"], date("F j, Y, g:i A", $currow["date"]), $currow["username"], $currow["body"]);
+      printf("<table cellspacing=\"0\" cellpadding=\"0\"><tr><td><table width=\"100%%\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\" ><tr><td><b>%s</b> - %s by %s</td></td></tr></table></tr><tr><td><table border=\"0\" cellspacing=\"0\" cellpadding=\"0\"><tr><td>%s</td></tr></table></td></tr></table><br>\n", $currow["headline"], date("F j, Y, g:i A", $currow["date"]), $currow["username"], $currow["body"]);
     }
     $i++;
   }
   if(mysql_num_rows($result) > 5) {
-    print "<br><div align=\"center\"><a href=\"/bzflag/news/old.php\">old news</a></div>\n";
+    print "<br><div align=\"center\"><a href=\"/bzflag/news/old.php\">old news</a></div>\n</td></tr></table>";
   }
 }
 
