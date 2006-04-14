416f7b3375425c3dc7cd450b51f221807b57e110
timriker <timriker@08b3d480-bf2c-0410-a26f-811ee3361c24>
2006-04-14 17:30:16 +0000

    knightmare
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@12822 08b3d480-bf2c-0410-a26f-811ee3361c24

--- db/weblogin.php
+++ db/weblogin.php
@@ -23,6 +23,7 @@ include('serversettings.php');
 # ip address. value is not used at present.
 # FIXME this should be in an sql table with a remote admin interface
 $banlist = array(
+  '68.109.43.46' => 'knightmare.kicks-ass.net',
 #  '127.0.0.1' => 'localhost'
   '255.255.255.255' => 'globalbroadcast'
 );
