2e8d52ed6828b41ec1ef548b733d1fa425f421f0
menotume <menotume@08b3d480-bf2c-0410-a26f-811ee3361c24>
2006-05-12 21:12:22 +0000

    Add define for mysql connection type.
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@12884 08b3d480-bf2c-0410-a26f-811ee3361c24

--- db/bzfls.php
+++ db/bzfls.php
@@ -12,6 +12,8 @@
 // IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
 // WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 
+define (MYSQL_PERSISTENT, false);  // user persistent connection ?
+
 # where to send debug printing (might override below)
 $debugLevel= 2;      // set to >2 to see all sql queries (>1 to see GET/POST input args)
 $debugFilename	= '/var/log/bzfls/bzfls.log';
@@ -756,8 +758,11 @@ if ($banlist[$_SERVER['REMOTE_ADDR']] != "") {
 debug('Connecting to the database', 3);
 
 # Connect to the server database persistently.
-$link = mysql_pconnect($dbhost, $dbuname, $dbpass)
-     or die('Could not connect: ' . mysql_error());
+if (MYSQL_PERSISTENT === true){
+  $link = mysql_pconnect($dbhost, $dbuname, $dbpass)  or die('Could not connect: ' . mysql_error());
+}else{
+  $link = mysql_connect($dbhost, $dbuname, $dbpass)  or die('Could not connect: ' . mysql_error());
+}
 if (!mysql_select_db($dbname)) {
   debug("Database $dbname did not exist", 1);
   die('Could not open db: ' . mysql_error());
