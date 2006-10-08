18dd168fac4e8f55e4d92b0b1875de213c0d7619
jeffm2501 <jeffm2501@08b3d480-bf2c-0410-a26f-811ee3361c24>
2006-10-08 22:19:36 +0000

    busted link
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@13306 08b3d480-bf2c-0410-a26f-811ee3361c24

--- web/getin/index.php
+++ web/getin/index.php
@@ -10,7 +10,6 @@ access. These pages might be of interest:<br><br>
 <a href="http://sourceforge.net/tracker/?atid=103248&group_id=3248&func=browse">Current bugs</a><br>
 <a href="http://sourceforge.net/tracker/?atid=303248&group_id=3248&func=browse">Current patches</a><br>
 <a href="http://sourceforge.net/tracker/?atid=353248&group_id=3248&func=browse">Feature requests</a><br>
-<a href="/developers/flags.php">TODO flags list</a><br>
 <br>
 end;
 $doc->end();
