4370697492ea4e5f93917458b7b4b03ef3fb3caf
judecn <judecn@08b3d480-bf2c-0410-a26f-811ee3361c24>
2007-06-01 00:27:50 +0000

    newly re-arranged tree
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@14251 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ bzworkbench/dialogs/MainMenu.cpp
@@ -0,0 +1,219 @@
+#include "../include/MainMenu.h"
+
+// builds the menu widgets
+void MainMenu::buildMenu(void) {
+	
+	this->add("Add Object", 0, 0, 0, FL_SUBMENU | FL_MENU_DIVIDER);
+		this->add("Add Object/Add box", FL_CTRL+'b', this->addBoxCallback, this);
+		this->add("Add Object/Add pyramid", FL_CTRL+'p', this->addPyramidCallback, this);
+		this->add("Add Object/Add teleporter", FL_CTRL+'t', this->addTeleporterCallback, this);
+		this->add("Add Object/Add sphere", 0, this->addSphereCallback, this);
+		this->add("Add Object/Add arc", 0, this->addArcCallback, this);
+		this->add("Add Object/Add mesh", FL_CTRL+'m', this->addMeshCallback, this);
+		this->add("Add Object/Add tetrahedron", 0, this->addTetrahedronCallback, this);
+		this->add("Add Object/Add torus", 0, this->addTorusCallback, this);
+		this->add("Add Object/Add cone", 0, this->addConeCallback, this);
+		this->add("Add Object/Add zone", 0, this->addZoneCallback, this);
+		this->add("Add Object/Add base", 0, 0, 0, FL_SUBMENU | FL_MENU_DIVIDER);
+			this->add("Add Object/Add base/Add Purple Base", 0, this->addPurpleBaseCallback, this);
+			this->add("Add Object/Add base/Add Red Base", 0, this->addRedBaseCallback, this);
+			this->add("Add Object/Add base/Add Green Base", 0, this->addGreenBaseCallback, this);
+			this->add("Add Object/Add base/Add Blue Base", 0, this->addBlueBaseCallback, this);
+		this->add("Add Object/Import object...", 0, this->importObjectCallback, this);
+	
+	this->add("Cut", FL_CTRL+'x', this->cutCallback, this);
+	this->add("Copy", FL_CTRL+'c', this->copyCallback, this);
+	this->add("Paste", FL_CTRL+'v', this->pasteCallback, this);
+	this->add("Duplicate", FL_CTRL+'d', this->cloneCallback, this);
+	this->add("Delete", FL_Delete, this->deleteCallback, this);
+	
+	this->add("(Un)Group Selection", FL_CTRL+'g', this->groupCallback, this);
+	this->add("(Un)Hide Selection", FL_CTRL+'h', this->hideCallback, this);
+	this->add("Save Selection As...", 0, this->saveSelectionCallback, this, FL_MENU_DIVIDER);
+	
+	this->add("Configure World...", 0, this->configureWorldCallback, this);
+	this->add("Configure Object...", FL_CTRL+'o', this->configureObjectCallback, this, FL_MENU_DIVIDER);
+	
+	this->add("Define World Weapon...", FL_CTRL+'w', this->worldWeaponCallback, this);
+	this->add("Link Teleporters", 0, this->linkCallback, this);
+	
+}
+
+
+// construct a menu at a given point
+MainMenu::MainMenu(int x, int y, int width, int height) :
+	Fl_Menu_Button(x, y, width, height) {
+	
+	this->buildMenu();
+	
+	this->type(Fl_Menu_Button::POPUP3);
+}
+
+// main destructor
+MainMenu::~MainMenu() { }
+
+// callbacks
+
+// add a box
+void MainMenu::addBoxCallback_real(Fl_Widget* w) {
+	printf("added a box\n");
+	this->value(0);
+}
+
+// add a pyramid
+void MainMenu::addPyramidCallback_real(Fl_Widget* w) {
+	printf("added a pyramid\n");
+	this->value(0);
+}
+
+// add a teleporter
+void MainMenu::addTeleporterCallback_real(Fl_Widget* w) {
+	printf("added a teleporter\n");
+	this->value(0);
+}
+
+// add a sphere
+void MainMenu::addSphereCallback_real(Fl_Widget* w) {
+	printf("added a sphere\n");
+	this->value(0);
+}
+
+// add an arc
+void MainMenu::addArcCallback_real(Fl_Widget* w) {
+	printf("added an arc\n");
+	this->value(0);
+}
+
+// add a mesh
+void MainMenu::addMeshCallback_real(Fl_Widget* w) {
+	printf("added a mesh\n");
+	this->value(0);
+}
+
+// add a tetrahedron
+void MainMenu::addTetrahedronCallback_real(Fl_Widget* w) {
+	printf("added a tetrahedron\n");
+	this->value(0);
+}
+
+// add a torus
+void MainMenu::addTorusCallback_real(Fl_Widget* w) {
+	printf("added a torus\n");
+	this->value(0);
+}
+
+// add a cone
+void MainMenu::addConeCallback_real(Fl_Widget* w) {
+	printf("added a cone\n");
+	this->value(0);
+}
+
+// add a zone
+void MainMenu::addZoneCallback_real(Fl_Widget* w) {
+	printf("added a zone\n");
+	this->value(0);
+}
+
+// import an object
+void MainMenu::importObjectCallback_real(Fl_Widget* w) {
+	printf("imported an object\n");
+	this->value(0);
+}
+
+// add base 1
+void MainMenu::addPurpleBaseCallback_real(Fl_Widget* w) {
+	printf("added purple base\n");
+	this->value(0);
+}
+
+// add base 2
+void MainMenu::addRedBaseCallback_real(Fl_Widget* w) {
+	printf("added red base\n");
+	this->value(0);
+}
+
+// add base 3
+void MainMenu::addGreenBaseCallback_real(Fl_Widget* w) {
+	printf("added green base\n");
+	this->value(0);
+}
+
+// add base 4
+void MainMenu::addBlueBaseCallback_real(Fl_Widget* w) {
+	printf("added blue base\n");
+	this->value(0);
+}
+
+// handle cut
+void MainMenu::cutCallback_real(Fl_Widget* w) {
+	printf("did a cut\n");
+	this->value(0);
+}
+
+// handle copy
+void MainMenu::copyCallback_real(Fl_Widget* w) {
+	printf("did a copy\n");
+	this->value(0);
+}
+
+// handle paste
+void MainMenu::pasteCallback_real(Fl_Widget* w) {
+	printf("did a paste\n");
+	this->value(0);
+}
+
+// handle selection clone
+void MainMenu::cloneCallback_real(Fl_Widget* w) {
+	printf("did a clone\n");
+	this->value(0);
+}
+
+// handle deletion
+void MainMenu::deleteCallback_real(Fl_Widget* w) {
+	printf("deletion!\n");
+	this->value(0);
+}
+
+// handle (un)grouping
+void MainMenu::groupCallback_real(Fl_Widget* w) {
+	printf("(un)grouped\n");
+	this->value(0);
+}
+
+// handle (un)hiding
+void MainMenu::hideCallback_real(Fl_Widget* w) {
+	printf("(un)hid\n");
+	this->value(0);
+}
+
+// handle save selection
+void MainMenu::saveSelectionCallback_real(Fl_Widget* w) {
+	printf("saved selection\n");
+	this->value(0);
+}
+
+// handle configure world
+void MainMenu::configureWorldCallback_real(Fl_Widget* w) {
+	
+	// MainWindow::openDialog(MainWindow::doWorldOptionsDialog);
+	printf("configured world\n");
+	this->value(0);
+}
+
+// handle world weapon creation
+void MainMenu::worldWeaponCallback_real(Fl_Widget* w) {
+	printf("world weapon created\n");
+	this->value(0);
+}
+
+// handle object configuration
+void MainMenu::configureObjectCallback_real(Fl_Widget* w) {
+	printf("configured an object\n");	
+	this->value(0);
+}
+
+// handle teleporter linking
+void MainMenu::linkCallback_real(Fl_Widget* w) {
+	printf("linkage\n");
+	this->value(0);
+}
---
+++ bzworkbench/include/Fl_Dialog.h
@@ -0,0 +1,70 @@
+#ifndef FL_DIALOG_H_
+#define FL_DIALOG_H_
+
+#include <FL/Fl.H>
+#include <FL/Fl_Widget.H>
+#include <FL/Fl_Button.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Group.H>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// This class is a simple dialog box with OK and CANCEL buttons, often used as the parent class of additional classes
+class Fl_Dialog : public Fl_Window
+{
+public:
+	
+	// bit masks that can be |-ed together in the flags paramter of some constructors
+	static const unsigned long Fl_OK = 0x1;
+	static const unsigned long Fl_CANCEL = 0x2;
+	
+	// constructors (see Fl_Dialog.cpp for details)
+	Fl_Dialog(const char* title);
+	Fl_Dialog(const char* title, unsigned long flags);
+	Fl_Dialog(const char* title, int width, int height, unsigned long flags);
+	
+	// destructor
+	virtual ~Fl_Dialog();
+	
+	// methods (see Fl_Dialog.cpp for details)
+	bool setOKEventHandler(void (*handler)(Fl_Widget* w, void* data), void* data);
+	bool setCancelEventHandler(void (*handler)(Fl_Widget* w, void* data), void* data);
+	
+private:
+	
+	// default window dimensions
+	static const int DEFAULT_WIDTH = 400;
+	static const int DEFAULT_HEIGHT = 300;
+	
+	// default dimensions of the OK button
+	static const int OK_DEFAULT_WIDTH = 75;
+	static const int OK_DEFAULT_HEIGHT = 20;
+	
+	// default dimensions of the CANCEL button
+	static const int CANCEL_DEFAULT_WIDTH = 75;
+	static const int CANCEL_DEFAULT_HEIGHT = 20; 
+	
+	// default coordinates of the OK/CANCEL button group
+	static const int DEFAULT_GROUP_X = 0;
+	static const int DEFAULT_GROUP_Y = DEFAULT_HEIGHT - 40;
+	
+	// default dimensions of the OK/CANCEL button group
+	static const int DEFAULT_GROUP_WIDTH = DEFAULT_WIDTH;
+	static const int DEFAULT_GROUP_HEIGHT = 40;
+	
+	// default group--contains the OK and CANCEL buttons
+	Fl_Group* defaultGroup;
+	
+	// not always used; pointers to OK and CANCEL button widgets
+	Fl_Button *OK_button, *CANCEL_button;
+	
+	// constructor helper methods
+	void doConstructor(int width, int height);
+	void finishConstructor(void);
+	void setUpButtons(unsigned long flags);
+};
+
+#endif /*FL_DIALOG_H_*/
