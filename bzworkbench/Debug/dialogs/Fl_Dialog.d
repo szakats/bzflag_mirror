c53506530835b2cca3101eabb4db93d4e2f79522
judecn <judecn@08b3d480-bf2c-0410-a26f-811ee3361c24>
2007-06-01 00:01:46 +0000

    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@14248 08b3d480-bf2c-0410-a26f-811ee3361c24

--- bzworkbench/Debug/dialogs/Fl_Dialog.d
+++
@@ -1,4 +0,0 @@
-dialogs/Fl_Dialog.d dialogs/Fl_Dialog.o: ../dialogs/Fl_Dialog.cpp \
-  ../dialogs/Fl_Dialog.h
-
-../dialogs/Fl_Dialog.h:
--- bzworkbench/Debug/dialogs/QuickLabel.d
+++
@@ -1,6 +0,0 @@
-dialogs/QuickLabel.d dialogs/QuickLabel.o: ../dialogs/QuickLabel.cpp \
-  ../dialogs/QuickLabel.h ../dialogs/../defines.h
-
-../dialogs/QuickLabel.h:
-
-../dialogs/../defines.h:
