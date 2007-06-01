d8bc7b4b6228822b7c3f70d8dd41093fb357a935
judecn <judecn@08b3d480-bf2c-0410-a26f-811ee3361c24>
2007-06-01 03:04:21 +0000

    Okay, this should be a complete commit
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@14258 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ bzworkbench/Debug/dialogs/Fl_Dialog.d
@@ -0,0 +1,4 @@
+dialogs/Fl_Dialog.d dialogs/Fl_Dialog.o: ../dialogs/Fl_Dialog.cpp \
+  ../dialogs/../include/Fl_Dialog.h
+
+../dialogs/../include/Fl_Dialog.h:
---
+++ bzworkbench/Debug/dialogs/MainMenu.d
@@ -0,0 +1,6 @@
+dialogs/MainMenu.d dialogs/MainMenu.o: ../dialogs/MainMenu.cpp \
+  ../dialogs/../include/MainMenu.h ../dialogs/../include/defines.h
+
+../dialogs/../include/MainMenu.h:
+
+../dialogs/../include/defines.h:
