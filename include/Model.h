d1a868dfe65b0dcf14651f48ec7471231a07a201
judecn <judecn@08b3d480-bf2c-0410-a26f-811ee3361c24>
2007-06-01 00:27:50 +0000

    newly re-arranged tree
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk/bzworkbench@14251 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ include/Model.h
@@ -0,0 +1,66 @@
+#ifndef MODEL_H_
+#define MODEL_H_
+
+class Model
+{
+public:
+	Model();
+	virtual ~Model();
+	
+	// every class the model stores must be derived from this:
+	class DataEntry {
+	public:
+		virtual void getData(Model* m);
+		virtual void updateData(Model* m);
+		
+		DataEntry() { }
+		virtual ~DataEntry() { }
+	};
+
+	class WorldOptionsData : public Model::DataEntry {
+		public:
+			WorldOptionsData(char* worldName, char* optionsString, float waterLevel, float flagHeight) {
+				update(worldName, optionsString, waterLevel, flagHeight);	
+			}
+			
+			// send the data
+			void get(char* worldName, char* optionsString, float& waterLevel, float& flagHeight) {
+				worldName = this->worldName;
+				optionsString = this->optionsString;
+				waterLevel = this->waterLevel;
+				flagHeight = this->flagHeight;	
+			}
+			
+			// receive the data
+			void update(char* worldName, char* optionsString, float waterLevel, float flagHeight) {
+				this->worldName = worldName;
+				this->optionsString = optionsString;
+				this->waterLevel = waterLevel;
+				this->flagHeight = flagHeight;	
+			}
+			
+			// methods to implement from DataEntry:
+			void getData(Model* h) {
+					
+			}
+			
+			void updateData(Model* h) {
+				
+			}
+			
+		private:
+			char* worldName;
+			char* optionsString;
+			float waterLevel;
+			float flagHeight;
+	};
+	
+private:
+// world options
+	WorldOptionsData* worldOptionsData;
+	
+};
+
+#include "WorldOptionsData.h"
+
+#endif /*MODEL_H_*/
