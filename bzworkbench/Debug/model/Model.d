801a91fa199a8b99fe069da2212ec6eda0b50a81
judecn <judecn@08b3d480-bf2c-0410-a26f-811ee3361c24>
2007-06-01 03:17:38 +0000

    no more binary
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@14260 08b3d480-bf2c-0410-a26f-811ee3361c24

--- bzworkbench/Debug/model/Model.d
+++ bzworkbench/Debug/model/Model.d
@@ -1,4 +1,9 @@
 model/Model.d model/Model.o: ../model/Model.cpp \
-  ../model/../include/Model.h
+  ../model/../include/Model.h ../model/../include/DataEntry.h \
+  ../model/../include/WorldOptionsData.h
 
 ../model/../include/Model.h:
+
+../model/../include/DataEntry.h:
+
+../model/../include/WorldOptionsData.h:
--- bzworkbench/include/Model.h
+++ bzworkbench/include/Model.h
@@ -1,63 +1,16 @@
 #ifndef MODEL_H_
 #define MODEL_H_
 
+class Model;
+
+#include "DataEntry.h"
+#include "WorldOptionsData.h"
+
 class Model
 {
 public:
 	Model();
 	virtual ~Model();
-	
-	// every class the model stores must be derived from this:
-	class DataEntry {
-	public:
-		virtual void getData(Model* m) = 0;
-		virtual void updateData(Model* m) = 0;
-		
-		DataEntry() { 
-		
-		}
-		virtual ~DataEntry() { 
-		
-		}
-	};
-
-	class WorldOptionsData : public Model::DataEntry {
-		public:
-			WorldOptionsData(char* worldName, char* optionsString, float waterLevel, float flagHeight) {
-				update(worldName, optionsString, waterLevel, flagHeight);	
-			}
-			
-			// send the data
-			void get(char* worldName, char* optionsString, float& waterLevel, float& flagHeight) {
-				worldName = this->worldName;
-				optionsString = this->optionsString;
-				waterLevel = this->waterLevel;
-				flagHeight = this->flagHeight;	
-			}
-			
-			// receive the data
-			void update(char* worldName, char* optionsString, float waterLevel, float flagHeight) {
-				this->worldName = worldName;
-				this->optionsString = optionsString;
-				this->waterLevel = waterLevel;
-				this->flagHeight = flagHeight;	
-			}
-			
-			// methods to implement from DataEntry:
-			void getData(Model* h) {
-					
-			}
-			
-			void updateData(Model* h) {
-				
-			}
-			
-		private:
-			char* worldName;
-			char* optionsString;
-			float waterLevel;
-			float flagHeight;
-	};
 	
 private:
 // world options
