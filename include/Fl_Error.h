43db85687a0dcab3ff2356bf2bc91e413b7de4ce
judecn <judecn@08b3d480-bf2c-0410-a26f-811ee3361c24>
2007-06-07 05:40:42 +0000

    added error box mechanism
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk/bzworkbench@14304 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ include/Fl_Error.h
@@ -0,0 +1,44 @@
+#ifndef FL_ERROR_H_
+#define FL_ERROR_H_
+
+#include <FL/Fl.H>
+#include <FL/Fl_Multiline_Output.H>
+#include <FL/Fl_XPM_Image.H>
+
+#include "Fl_Dialog.h"
+#include "QuickLabel.h"
+
+class Fl_Error : public Fl_Dialog {
+
+public:
+	// build the error box
+	Fl_Error(const char* message);
+	
+	virtual ~Fl_Error();
+	
+	// set error text
+	void setText(const char* message);
+	
+	// close callback
+	static void Fl_ErrorCallback(Fl_Widget* w, void* data) {
+		Fl_Error* dialog = (Fl_Error*)(data);
+		dialog->Fl_ErrorCallback_real(w);	
+	}
+	
+private:
+
+	// default callback
+	void Fl_ErrorCallback_real(Fl_Widget* w);
+	
+	// error message
+	Fl_Multiline_Output* errorMessage;
+	
+	// error icon
+	Fl_XPM_Image* errorImage;
+	
+	// error label
+	QuickLabel* errorLabel;
+	
+};
+
+#endif /*FL_ERROR_H_*/
