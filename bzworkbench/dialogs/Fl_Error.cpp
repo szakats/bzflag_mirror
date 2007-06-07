16bc88a7e22ab547f1033f42cb43661a5b3749fb
judecn <judecn@08b3d480-bf2c-0410-a26f-811ee3361c24>
2007-06-07 05:40:42 +0000

    added error box mechanism
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@14304 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ bzworkbench/dialogs/Fl_Error.cpp
@@ -0,0 +1,33 @@
+#include "../include/Fl_Error.h"
+
+Fl_Error::Fl_Error(const char* message) :
+	Fl_Dialog("BZWorkbench Error", DEFAULT_WIDTH, 200, Fl_Dialog::Fl_OK) {
+		
+	this->errorImage = new Fl_XPM_Image("share/error.xpm");
+	
+	this->errorMessage = new Fl_Multiline_Output(60, 20, DEFAULT_WIDTH - 60 - 10, 200 - 30 - 10, "An error has occurred:");
+	this->errorMessage->align(FL_ALIGN_TOP | FL_ALIGN_LEFT);
+	this->errorMessage->value(message);
+	
+	this->errorLabel = new QuickLabel("", 5, 100 - 24);
+	this->errorLabel->image(errorImage);
+	
+	this->add(errorLabel);
+	this->add(errorMessage);
+	
+	this->setOKEventHandler(Fl_ErrorCallback, this);
+	
+	this->redraw();
+}
+
+Fl_Error::~Fl_Error() {
+	if(this->errorImage)
+		delete errorImage;
+}
+
+void Fl_Error::setText(const char* message) { this->errorMessage->value(message); }
+
+// default callbacks
+void Fl_Error::Fl_ErrorCallback_real(Fl_Widget* w) {
+	Fl::delete_widget(this);
+}
