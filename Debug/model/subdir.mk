0b30a00d0b7db3bd02379cea6b0b7494f4bab218
judecn <judecn@08b3d480-bf2c-0410-a26f-811ee3361c24>
2007-06-14 05:46:59 +0000

    fixed up makefiles...?
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk/bzworkbench@14331 08b3d480-bf2c-0410-a26f-811ee3361c24

--- Debug/model/subdir.mk
+++ Debug/model/subdir.mk
@@ -4,16 +4,16 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../model/Model.cpp \
-../model/BZWParser.cpp
+../model/BZWParser.cpp \
+../model/Model.cpp 
 
 OBJS += \
-./model/Model.o \
-./model/BZWParser.o
+./model/BZWParser.o \
+./model/Model.o 
 
 CPP_DEPS += \
-./model/Model.d \
-./model/BZWParser.d
+./model/BZWParser.d \
+./model/Model.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
