289e0d6ef1823943f73940b3a03ce9b4e0a75db1
judecn <judecn@08b3d480-bf2c-0410-a26f-811ee3361c24>
2007-07-17 06:06:53 +0000

    Still some issues with copying and pasting, but cutting and deleting objects now works, and opening and saving .bzw files works. We're really close to having a box-only editor in place; just a few more changes are needed.  Overall, I'd say I'm back on schedule :)
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@14615 08b3d480-bf2c-0410-a26f-811ee3361c24

--- bzworkbench/include/objects/box.h
+++ bzworkbench/include/objects/box.h
@@ -25,10 +25,10 @@ public:
 	// constructor with data
 	box(string& data);
 	
-	static DataEntry* init() { return new box( osg::Vec3( 0.0, 0.0, 0.0 ), 0.0f, osg::Vec3( 0.0, 0.0, 0.0 ) ); }
+	static DataEntry* init() { return new box( osg::Vec3( 0.0, 0.0, 0.0 ), 0.0f, osg::Vec3( 10.0, 10.0, 10.0 ) ); }
 	static DataEntry* init(string& data) {
 		if( data.size() <= 1 )
-			return new box( osg::Vec3( 0.0, 0.0, 0.0 ), 0.0f, osg::Vec3( 0.0, 0.0, 0.0 ) );
+			return new box( osg::Vec3( 0.0, 0.0, 0.0 ), 0.0f, osg::Vec3( 10.0, 10.0, 10.0 ) );
 		else
 			return new box(data);
 	}
