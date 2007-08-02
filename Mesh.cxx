7a2ae417f5b9d775ba5da22ad92395c59b165ab9
chaos-dev <chaos-dev@08b3d480-bf2c-0410-a26f-811ee3361c24>
2007-08-02 00:55:44 +0000

    Implemented extrudeFace and a few helper methods
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk/bzwgen@14739 08b3d480-bf2c-0410-a26f-811ee3361c24

--- Mesh.cxx
+++ Mesh.cxx
@@ -12,17 +12,16 @@
 
 #include "Mesh.h"
 
-int Mesh::createNewFace(Vertex a, Vertex b, Vertex c, Vertex d) {
+int Mesh::createNewFace(Vertex a, Vertex b, Vertex c, Vertex d, int mat) {
   v.push_back(a);
   v.push_back(b);
   v.push_back(c);
   v.push_back(d);
   int size = v.size();
-  f.push_back(Face(ID4(size-4,size-3,size-2,size-1),ID4()));
-  return f.size()-1;
+  return addFace(Face(ID4(size-4,size-3,size-2,size-1),mat));
 }
 
-int Mesh::createNewFace(Vertex a, Vertex b, Vertex c, Vertex d, TexCoord tca, TexCoord tcb, TexCoord tcc, TexCoord tcd) {
+int Mesh::createNewFace(Vertex a, Vertex b, Vertex c, Vertex d, TexCoord tca, TexCoord tcb, TexCoord tcc, TexCoord tcd, int mat) {
   v.push_back(a);
   v.push_back(b);
   v.push_back(c);
@@ -33,18 +32,43 @@ int Mesh::createNewFace(Vertex a, Vertex b, Vertex c, Vertex d, TexCoord tca, Te
   tc.push_back(tcc);
   tc.push_back(tcd);
   int tsize = tc.size();
-  f.push_back(Face(ID4(size-4,size-3,size-2,size-1),ID4(tsize-4,tsize-3,tsize-2,tsize-1)));
-  return f.size()-1;
+  return addFace(Face(ID4(size-4,size-3,size-2,size-1),ID4(tsize-4,tsize-3,tsize-2,tsize-1),mat));
 }
+
+ID4 Mesh::extrudeFace(int fid, float amount) {
+  Vertex dir = faceNormal(fid)*amount;
+  ID4 base = f[fid].vtx;
+  int newface = createNewFace(v[base.a]+dir,v[base.b]+dir,v[base.c]+dir,v[base.d]+dir,f[fid].mat);
+  f[fid] = f[newface];
+  f.pop_back();
+  ID4 top = f[fid].vtx;
+  return ID4(
+      addFace(Face(ID4(base[0],base[1],top[1],top[0]))),
+      addFace(Face(ID4(base[1],base[2],top[2],top[1]))),
+      addFace(Face(ID4(base[2],base[3],top[3],top[2]))),
+      addFace(Face(ID4(base[3],base[0],top[0],top[3])))
+    );
+}
+
+Vertex Mesh::faceNormal(int fid) {
+  Face face = f[fid];
+  Vertex a = v[face.vtx.a]-v[face.vtx.b];
+  Vertex b = v[face.vtx.a]-v[face.vtx.c];
+  Vertex r = a.cross(b);
+  int length = r.length();
+  return r / length;
+}
+
 
 void Mesh::output(Output& out) {
   out.line("mesh");
-  out.matref(matref); // temp
+  out.matref(matref); 
+  int mat = matref;
   for (size_t i = 0; i < v.size(); i++) out.vertex(v[i]);
   for (size_t i = 0; i < tc.size(); i++) out.texcoord(tc[i]);
   if (passable) out.line("  passable"); 
   if (noradar) out.line("  noradar"); 
-  for (size_t i = 0; i < f.size(); i++) out.face(f[i]);
+  for (int i = 0; i < f.size(); i++) out.face(f[i],mat);
   out.line("end\n");
 }
 
