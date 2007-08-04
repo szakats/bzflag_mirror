709133842abab2a3bdb0047a19e7871e82922625
chaos-dev <chaos-dev@08b3d480-bf2c-0410-a26f-811ee3361c24>
2007-08-04 22:50:31 +0000

    Face as a class. Extended the implementation to allow support for non-quad faces.
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk/bzwgen@14819 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ Face.h
@@ -0,0 +1,61 @@
+/* bzflag
+ * Copyright (c) 1993 - 2006 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#ifndef __FACE_H__
+#define __FACE_H__
+
+#include "common.h"
+#include "Output.h"
+
+class Face {
+public:
+  IntVector* vtx;
+  IntVector* tcd;
+  int mat;
+  bool texcoords;
+  Face() : mat(0), texcoords(true) {
+    vtx = new IntVector;
+    tcd = new IntVector;
+  }
+  Face(ID4 _vtx, ID4 _tcd, int _mat = 0) : mat(_mat), texcoords(true) {
+    vtx = new IntVector;
+    tcd = new IntVector;
+    for (int i = 0; i < 4; i++) {
+      vtx->push_back(_vtx[i]);
+      tcd->push_back(_tcd[i]);
+    }
+  }
+  Face(ID4 _vtx, int _mat = 0) : mat(_mat), texcoords(false) {
+    vtx = new IntVector;
+    tcd = new IntVector;
+    for (int i = 0; i < 4; i++) {
+      vtx->push_back(_vtx[i]);
+    }
+  }
+  ~Face() {
+    delete vtx;
+    delete tcd;
+  }
+};
+
+typedef std::vector<Face> FaceVector;
+typedef FaceVector::iterator FaceVectIter;
+
+#endif /* __FACE_H__ */
+
+// Local Variables: ***
+// mode:C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
