517c5d3740bd103f42bf61ca9e8edad7a7b18ba0
chaos-dev <chaos-dev@08b3d480-bf2c-0410-a26f-811ee3361c24>
2007-08-16 16:54:07 +0000

    Rule class stub.
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@14933 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ bzwgen/Rule.h
@@ -0,0 +1,33 @@
+/* bzflag
+ * Copyright (c) 1993 - 2006 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#ifndef __RULE_H__
+#define __RULE_H__
+
+#include <string>
+
+class Rule {
+  std::string name;
+public:
+  Rule(const std::string& _name) : name(_name) {};
+};
+
+
+#endif /* __RULE_H__ */
+
+// Local Variables: ***
+// mode:C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
