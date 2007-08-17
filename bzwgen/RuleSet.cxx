f1041d39e4ad48e20101b3b578b0ebd2188b37ca
chaos-dev <chaos-dev@08b3d480-bf2c-0410-a26f-811ee3361c24>
2007-08-17 23:08:01 +0000

    Darn, forgot to svn add RuleSet.cxx :/
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@15050 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ bzwgen/RuleSet.cxx
@@ -0,0 +1,41 @@
+/* bzflag
+ * Copyright (c) 1993 - 2006 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#include "RuleSet.h"
+
+int RuleSet::runMesh(Mesh* mesh, int face, std::string& rulename) {
+  if (recursion == -1) return -1;
+  recursion++;
+  if (recursion == MAX_RECURSION) {
+    recursion = -1;
+    printf("\nRecursion level 1000 reached! Are you sure you have no infinite loops?\n");
+    return -1;
+  }
+  RuleMapIter itr = rules.find(rulename); 
+  if (itr == rules.end()) return -1;
+  int result = itr->second->runMesh(mesh,face);
+  recursion--;
+  return result;
+}
+
+RuleSet::~RuleSet() { 
+  RuleMapIter itr;
+  for (itr = rules.begin();itr != rules.end(); ++itr) delete itr->second; 
+}
+
+// Local Variables: ***
+// mode:C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
