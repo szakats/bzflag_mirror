84b13f26248de83e3e2db9c5df7fbe9284b472e0
chaos-dev <chaos-dev@08b3d480-bf2c-0410-a26f-811ee3361c24>
2007-08-20 00:59:15 +0000

    Introducing BaseZone
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@15186 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ bzwgen/BaseZone.h
@@ -0,0 +1,48 @@
+/* bzflag
+ * Copyright (c) 1993 - 2006 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+
+#ifndef __BASEZONE_H__
+#define __BASEZONE_H__
+
+#include <vector>
+#include <fstream>
+#include <string>
+#include "globals.h"
+#include "Zone.h"
+#include "Mesh.h"
+
+
+class BaseZone : public Zone {
+protected:
+  int color;
+public:
+  BaseZone(Generator* _generator, Coord2D a, Coord2D b) : Zone(_generator,a,b,0) {};
+  int setColor(int _color) { color = _color; }
+  virtual void output(Output& out) {
+    out << "base\n";
+    out << "  position " << (A.x+B.x)/2 << " " << (A.y+B.y)/2 << " 0\n";
+    out << "  size " << abs(A.x-B.x)/2 << " " << abs(A.y-B.y)/2 << " 0\n";
+    out << "  color " << color << "\n";
+    out << "end\n\n";
+  }
+};
+
+#endif /* __BASEZONE_H__ */
+
+// Local Variables: ***
+// mode:C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
