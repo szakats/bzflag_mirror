b0b6e8e55c6da6d70021ba4dbd6f179860cde8ce
chaos-dev <chaos-dev@08b3d480-bf2c-0410-a26f-811ee3361c24>
2007-08-28 08:03:09 +0000

    New face attribute 'n' returning the number of vertices
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@15532 08b3d480-bf2c-0410-a26f-811ee3361c24

--- bzwgen/src/Expression.cxx
+++ bzwgen/src/Expression.cxx
@@ -24,6 +24,7 @@ double ExpressionFaceAttribute::calculate(Mesh* mesh,int face) {
   if (attrname == "h") return mesh->faceH(face);
   if (attrname == "v") return mesh->faceV(face);
   if (attrname == "s") return mesh->faceH(face) * mesh->faceV(face);
+  if (attrname == "n") return double(mesh->f[face]->size());
   if (attrname == "c") {
     if (mesh->f[face]->isMultiFace()) {
       return double(((MultiFace*)mesh->f[face])->componentCount());
