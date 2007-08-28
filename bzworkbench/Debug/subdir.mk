60d39c64dc7ab3f3084cb4c739e9dc01848b3ac3
JeffM2501 <JeffM2501@08b3d480-bf2c-0410-a26f-811ee3361c24>
2007-08-28 21:43:58 +0000

    includetastic
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@15551 08b3d480-bf2c-0410-a26f-811ee3361c24

--- bzworkbench/Debug/subdir.mk
+++ bzworkbench/Debug/subdir.mk
@@ -26,7 +26,7 @@ CXX_DEPS += \
 src/%.o: ../src/%.cxx
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I/usr/include/ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	g++ -I/usr/include/ -I./include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
