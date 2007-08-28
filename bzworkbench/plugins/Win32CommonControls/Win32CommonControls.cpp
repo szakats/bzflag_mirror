501709aba56b005f755f071d34cc59eea82125c4
JeffM2501 <JeffM2501@08b3d480-bf2c-0410-a26f-811ee3361c24>
2007-08-28 21:57:41 +0000

    start a plugin for BZWB
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@15553 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ bzworkbench/plugins/Win32CommonControls/Win32CommonControls.cpp
@@ -0,0 +1,66 @@
+// Win32CommonControls.cpp : Defines the initialization routines for the DLL.
+//
+
+#ifndef VC_EXTRALEAN
+#define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
+#endif
+
+#ifndef WINVER				// Allow use of features specific to Windows 95 and Windows NT 4 or later.
+#define WINVER 0x0400		// Change this to the appropriate value to target Windows 98 and Windows 2000 or later.
+#endif
+
+#ifndef _WIN32_WINNT		// Allow use of features specific to Windows NT 4 or later.
+#define _WIN32_WINNT 0x0400	// Change this to the appropriate value to target Windows 2000 or later.
+#endif						
+
+#ifndef _WIN32_WINDOWS		// Allow use of features specific to Windows 98 or later.
+#define _WIN32_WINDOWS 0x0410 // Change this to the appropriate value to target Windows Me or later.
+#endif
+
+#ifndef _WIN32_IE			// Allow use of features specific to IE 4.0 or later.
+#define _WIN32_IE 0x0400	// Change this to the appropriate value to target IE 5.0 or later.
+#endif
+
+#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS	// some CString constructors will be explicit
+
+#include <afxwin.h>         // MFC core and standard components
+#include <afxext.h>         // MFC extensions
+
+#ifndef _AFX_NO_OLE_SUPPORT
+#include <afxole.h>         // MFC OLE classes
+#include <afxodlgs.h>       // MFC OLE dialog classes
+#include <afxdisp.h>        // MFC Automation classes
+#endif // _AFX_NO_OLE_SUPPORT
+
+#ifndef _AFX_NO_DB_SUPPORT
+#include <afxdb.h>			// MFC ODBC database classes
+#endif // _AFX_NO_DB_SUPPORT
+
+#ifndef _AFX_NO_DAO_SUPPORT
+#include <afxdao.h>			// MFC DAO database classes
+#endif // _AFX_NO_DAO_SUPPORT
+
+#include <afxdtctl.h>		// MFC support for Internet Explorer 4 Common Controls
+#ifndef _AFX_NO_AFXCMN_SUPPORT
+#include <afxcmn.h>			// MFC support for Windows Common Controls
+#endif // _AFX_NO_AFXCMN_SUPPORT
+
+#include "resource.h"		// main symbols
+
+#include "BZWBAPI.h"
+
+BZWB_GET_PLUGIN_VERSION
+
+HWND	theAppWindow = NULL;
+
+BZWB_PLUGIN_CALL int bzwb_Load (const char* /*commandLine*/)
+{
+	theAppWindow = (HWND) bzwb_getOSMainWindowHandle();
+	return 0;
+}
+
+BZWB_PLUGIN_CALL int bzwb_Unload (void)
+{
+	return 0;
+}
+
