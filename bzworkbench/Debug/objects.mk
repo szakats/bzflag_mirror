549076c49dd1573c707122b897d728a529429db8
dtremenak <dtremenak@08b3d480-bf2c-0410-a26f-811ee3361c24>
2007-09-27 20:15:50 +0000

    link with fltk_png and zlib
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@15970 08b3d480-bf2c-0410-a26f-811ee3361c24

--- bzworkbench/Debug/objects.mk
+++ bzworkbench/Debug/objects.mk
@@ -4,4 +4,4 @@
 
 USER_OBJS :=
 
-LIBS := -l"fltk" -l"fltk_images" -l"osg" -l"osgViewer" -l"fltk_gl" -l"GL" -l"GLU"
+LIBS := -lfltk -lfltk_images -lfltk_png -losg -lz -losgViewer -lfltk_gl -lGL -lGLU
