5c7b470664cb2a57f852fc08916ccc26c76df5e9
blast007 <blast007@08b3d480-bf2c-0410-a26f-811ee3361c24>
2007-10-17 04:09:19 +0000

    255 is a custom license, not 0
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@16050 08b3d480-bf2c-0410-a26f-811ee3361c24

--- web/submitimages/database.sql
+++ web/submitimages/database.sql
@@ -7,7 +7,7 @@ CREATE TABLE `img_queue` (
   `uploaderlastname` VARCHAR( 255 ) NOT NULL,
   `filename` VARCHAR( 255 ) NOT NULL,
   `authorname` VARCHAR( 255 ) NOT NULL,
-  `licenseid` TINYINT UNSIGNED NOT NULL COMMENT 'If 0, it is custom',
+  `licenseid` TINYINT UNSIGNED NOT NULL COMMENT 'If 255, it is custom',
   `licensename` VARCHAR( 255 ) NOT NULL,
   `licenseurl` VARCHAR( 255 ) NOT NULL,
   `licensebody` MEDIUMTEXT NOT NULL,
