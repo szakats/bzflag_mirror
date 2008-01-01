a3b25296824e241627da31257af5c8bc543d0a76
macsforme <macsforme@08b3d480-bf2c-0410-a26f-811ee3361c24>
2008-01-01 21:25:57 +0000

    SQL structure change: all permissions are now stored in an SQL table. Support for "special" permissions (i.e., members of one group can administer members in another on a group-to-group basis). Misc. data and auth class enhancements. Index page now displays users' memberships and admin-able groups for logged-in users.
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@16391 08b3d480-bf2c-0410-a26f-811ee3361c24

--- db/bzgrpmgr/index.php
+++ db/bzgrpmgr/index.php
@@ -6,7 +6,7 @@ require_once( 'include/global.inc' );
 // Page content
 require_once( 'template/header.inc' );
 
-// ==================== General Info ====================
+// ==================== General info ====================
 ?>
 
 <p>
@@ -29,6 +29,137 @@ Please log in to manage your groups.
 </p>
 
 <?php
+// ==================== My memberships ====================
+
+if( $auth->isLoggedIn() ) {
+	?>
+<p>
+
+<span class="large">My Memberships</span><br>
+
+<?php
+
+$groupids = $data->getGroupsByUser( $auth->getUserID() );
+if( count( $groupids ) == 0 )
+	echo "You are not a member of any groups at this time.<br>\n";
+else {
+	echo "<table>\n";
+	echo "<tr><th>Group</th><th>Members</th></tr>\n";
+
+	foreach( $groupids as $groupid )
+		// Form an array of ORG.GROUP names to ids, so we can sort alphabetically
+		$group_array[$data->getOrgName( $data->getGroupOrg( $groupid ) ).".".$data->getGroupName( $groupid )] = $groupid;
+	asort( $group_array );
+
+	foreach( $group_array as $groupname => $groupid )
+		echo "<tr><td>".$groupname."</td><td>".count( $data->getGroupMembers( $groupid ) )."</td></tr>\n";
+
+	echo "</table>\n";
+}
+?>
+
+</p>
+
+<?php
+}
+
+// ==================== Org and group data ====================
+
+if( $auth->isLoggedIn() ) {
+	?>
+<p>
+
+<span class="large">My Groups</span><br>
+
+<?php
+
+/*
+++ Organization Name<info/admin link> ++
+++++ Group Name<members link> ++++ <admin> ++++ <delete> ++++ (adminlevel) ++++
+++++ Add a group ++++
+++ Add an organization ++
+*/
+
+// Generate array of orgid's[groupid's]
+$orgs = array();
+foreach( $data->getGroupsByUser( $auth->getUserID() ) as $membergroupid ) {
+	$orgid = $data->getOrg( $membergroupid );
+	$org_groups = $data->getOrgGroups( $orgid );
+
+	// $data->isGroupAdmin() is expensive, so we'll use another
+	if( $data->isOrgAdminGroup( $membergroupid ) ) {
+		// no need to bother with each group... they're all admined,
+		// so add them all
+		foreach( $org_groups as $groupid ) {
+			if( ! $orgs[$orgid] )
+				$orgs[$orgid] = array();
+
+			// Use a hash here so we don't get duplicate group ids
+			$orgs[$orgid][$groupid] = 1;
+		}
+	} else if( $data->isSpecialAdminGroup( $membergroupid ) ) {
+		foreach( $org_groups as $groupid ) {
+			// See if this is the one we have special perms on
+			if( $data->isSpecialAdminGroup( $membergroupid, $groupid ) ) {
+				if( ! $orgs[$orgid] )
+					$orgs[$orgid] = array();
+
+				// Use a hash here so we don't get duplicate group ids
+				$orgs[$orgid][$groupid] = 1;
+			}
+		}
+	}
+}
+
+if( count( $orgs ) == 0 )
+	echo "You do not administer any groups at this time.<br>\n";
+else {
+	echo "<table>\n";
+	echo "<tr><th colspan=\"3\">Name</th><th colspan=\"2\">&nbsp;</th></tr>\n";
+
+	foreach( $orgs as $orgid => $group_array ) {
+		// Org info
+		echo "<tr class=\"org\"><td colspan=\"3\">".$data->getOrgName( $orgid )."</td>".
+				"<td>".( $data->isOrgAdmin( $auth->getUserID(), $orgid ) ?
+						"<img src=\"img/wrench.png\">" : "&nbsp;" )."</td>".
+				"<td colspan=\"2\">&nbsp;</td></tr>\n";
+
+		// Group name and links (if applicable)
+		ksort( $group_array );
+		foreach( array_keys( $group_array ) as $groupid )
+			echo "<tr><td>".
+					( $data->isOrgAdminGroup( $groupid, $orgid ) ? "<img src=\"img/key_high.png\">" :
+							( $data->isSpecialAdminGroup( $groupid ) ?
+									"<img src=\"img/key_low.png\">" : "&nbsp;" ) )."</td>".
+					"<td>&nbsp;</td>".
+					"<td>".$data->getGroupName( $groupid )."</td>".
+					( $data->isGroupAdmin( $auth->getUserID(), $groupid ) ?
+							"<td style=\"padding: 0\"><img src=\"img/wrench.png\"></td>".
+							"<td><img src=\"img/delete.png\"></td>" :
+							"<td>&nbsp;</td><td>&nbsp;</td>" ).
+					"</tr>\n";
+
+		// Final row for creating a new group
+		if( $data->isOrgAdmin( $auth->getUserID(), $orgid ) )
+			echo "<tr>".
+					"<td>&nbsp;</td><td>&nbsp;</td>".
+					"<td>New Group</td>".
+					"<td><img src=\"img/new.png\"></td>".
+					"<td>&nbsp;</td></tr>\n";
+	}
+
+	echo "</table>\n";
+}
+
+?>
+
+<br><a href="">Click here to register a new organization.</a>
+
+</p>
+
+<?php
+}
+
 // ==================== Site Information ====================
 ?>
 
@@ -36,6 +167,8 @@ Please log in to manage your groups.
 <span class="large">Site Information</span><br>
 Total Organizations: <?php echo $data->getNumOrgs(); ?><br>
 Total Groups: <?php echo $data->getNumGroups(); ?><br>
+Current Users:<br>
+Users Today:
 </span>
 </p>
 
@@ -257,57 +390,6 @@ ENCLOSE;
 
 		break;
 	default:
-		$output .= "\t\t<b>Groups:</b>\n";
-
-		$orgs = array();
-		foreach( $data->getGroups( $userdata['bzid'] ) as $groupid ) {
-			// Generate array of orgid's->groupid's
-			$orgid = $data->getOrg( $groupid );
-			$orgs[$orgid] = array();
-
-			foreach( $data->getOrgGroups( $orgid ) as $groupid )
-				array_push( $orgs[$orgid], $groupid );
-		}
-
-		// Output orgs, groups, and associated links
-		$output .= "\t\t<table>\n";
-		foreach( $orgs as $orgid => $group_array ) {
-
-			// Org name & admin link (if applicable)
-			$output .= "\t\t\t<tr><td colspan=\"4\">";
-			if( $data->isGroupAdmin( $orgid, $userdata['bzid'] ) )
-				$output .= "<a href=\"?action=orgadmin&id=".$orgid."\">".
-						$data->getOrgName( $orgid )."</a>";
-			else
-				$output .= $data->getOrgName( $orgid );
-			$output .= "</td></tr>\n";
-
-			// Group name and links (if applicable)
-			foreach( $group_array as $groupid ) {
-				$output .= "\t\t\t<tr><td>&nbsp;</td>";
-				$output .= "<td><a href=\"?action=showmembers&id=".$groupid."\">";
-				$output .= $data->getGroupName( $groupid );
-				$output .= "</a></td>";
-				$output .= ( $data->isGroupAdmin( $orgid, $userdata['bzid'] ) ?
-						"<td><a href=\"?action=editgroup&groupid=".$groupid."\">".
-								"Settings</a></td>" :
-//						"<td><a href=\"?action=groupdelete&id=".$groupid."\">".
-//								"Delete</a></td>"
-						"<td colspan=\"3\">&nbsp;</td>" ).
-						"</tr>\n";
-			}
-
-			// Final row for creating a new group
-			if( $data->isGroupAdmin( $orgid, $userdata['bzid'] ) )
-				$output .= "\t\t\t<tr><td>&nbsp;</td><td colspan=\"2\">".
-						"<a href=\"?action=editgroup&orgid=".
-						$orgid."\"><i>New Group</i></a></td></tr>\n";
-		}
-		// Final row for creating a new organization
-		$output .= "\t\t\t<tr><td colspan=\"3\"><a href=\"?action=createorg\"><i>New Organization".
-				"</i></a></td></tr>\n";
-		$output .= "\t\t</table>\n";
-
 		break;
 }
 
--- db/bzgrpmgr/setup.sql
+++ db/bzgrpmgr/setup.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: bzgrpmgr
 -- ------------------------------------------------------
--- Server version	5.0.38-Ubuntu_0ubuntu1-log
+-- Server version	5.0.45-Debian_1ubuntu3.1-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,46 +16,114 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `group_members`
+-- Table structure for table `groups_groups`
 --
 
-DROP TABLE IF EXISTS `group_members`;
-CREATE TABLE `group_members` (
-  `id` int(2) NOT NULL auto_increment,
-  `userid` int(2) NOT NULL,
-  `groupid` int(2) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
-
---
--- Table structure for table `groups`
---
-
-DROP TABLE IF EXISTS `groups`;
-CREATE TABLE `groups` (
+DROP TABLE IF EXISTS `groups_groups`;
+CREATE TABLE `groups_groups` (
   `groupid` int(2) unsigned NOT NULL auto_increment,
   `groupname` varchar(25) NOT NULL,
   `orgid` int(2) unsigned NOT NULL,
   `state` enum('open','closed','hidden','inactive') NOT NULL default 'inactive',
-  `adminusers` tinyint(1) NOT NULL default '0',
-  `admingroups` tinyint(1) NOT NULL default '0',
-  `admin` tinyint(1) NOT NULL default '0',
+  `adminlevel` tinyint(1) NOT NULL default '0',
+  `can_manage` int(2) default NULL,
+  `freeleave` tinyint(1) NOT NULL default '0',
   `description` varchar(128) default NULL,
   PRIMARY KEY  (`groupid`)
-) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
--- Table structure for table `organizations`
+-- Table structure for table `groups_memberships`
 --
 
-DROP TABLE IF EXISTS `organizations`;
-CREATE TABLE `organizations` (
+DROP TABLE IF EXISTS `groups_memberships`;
+CREATE TABLE `groups_memberships` (
+  `id` int(2) NOT NULL auto_increment,
+  `userid` int(2) NOT NULL,
+  `groupid` int(2) NOT NULL,
+  `pending` tinyint(1) NOT NULL default '0',
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `groups_orgs`
+--
+
+DROP TABLE IF EXISTS `groups_orgs`;
+CREATE TABLE `groups_orgs` (
   `orgid` int(2) NOT NULL auto_increment,
   `orgname` varchar(25) NOT NULL,
   `contact` int(2) NOT NULL,
   PRIMARY KEY  (`orgid`),
   UNIQUE KEY `orgname` (`orgname`)
-) ENGINE=MyISAM AUTO_INCREMENT=31 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `phpbb2_users`
+--
+
+DROP TABLE IF EXISTS `phpbb2_users`;
+CREATE TABLE `phpbb2_users` (
+  `user_id` mediumint(8) NOT NULL default '0',
+  `user_active` tinyint(1) default '1',
+  `username` varchar(25) NOT NULL default '',
+  `user_password` varchar(32) NOT NULL default '',
+  `user_session_time` int(11) NOT NULL default '0',
+  `user_session_page` smallint(5) NOT NULL default '0',
+  `user_lastvisit` int(11) NOT NULL default '0',
+  `user_regdate` int(11) NOT NULL default '0',
+  `user_level` tinyint(4) default NULL,
+  `user_posts` mediumint(8) unsigned NOT NULL default '0',
+  `user_timezone` decimal(5,2) NOT NULL default '0.00',
+  `user_style` tinyint(4) default NULL,
+  `user_lang` varchar(255) default NULL,
+  `user_dateformat` varchar(14) NOT NULL default 'd M Y H:i',
+  `user_new_privmsg` smallint(5) unsigned NOT NULL default '0',
+  `user_unread_privmsg` smallint(5) unsigned NOT NULL default '0',
+  `user_last_privmsg` int(11) NOT NULL default '0',
+  `user_emailtime` int(11) default NULL,
+  `user_viewemail` tinyint(1) default NULL,
+  `user_attachsig` tinyint(1) default NULL,
+  `user_allowhtml` tinyint(1) default '1',
+  `user_allowbbcode` tinyint(1) default '1',
+  `user_allowsmile` tinyint(1) default '1',
+  `user_allowavatar` tinyint(1) NOT NULL default '1',
+  `user_allow_pm` tinyint(1) NOT NULL default '1',
+  `user_allow_posting` tinyint(1) NOT NULL default '1',
+  `user_allow_viewonline` tinyint(1) NOT NULL default '1',
+  `user_notify` tinyint(1) NOT NULL default '0',
+  `user_notify_pm` tinyint(1) NOT NULL default '1',
+  `user_popup_pm` tinyint(1) NOT NULL default '0',
+  `user_rank` int(11) default NULL,
+  `user_avatar` varchar(100) default NULL,
+  `user_avatar_type` tinyint(4) NOT NULL default '0',
+  `user_email` varchar(255) default NULL,
+  `user_icq` varchar(15) default NULL,
+  `user_website` varchar(100) default NULL,
+  `user_from` varchar(100) default NULL,
+  `user_sig` text,
+  `user_sig_bbcode_uid` varchar(10) default NULL,
+  `user_aim` varchar(255) default NULL,
+  `user_yim` varchar(255) default NULL,
+  `user_msnm` varchar(255) default NULL,
+  `user_occ` varchar(100) default NULL,
+  `user_interests` varchar(255) default NULL,
+  `user_actkey` varchar(32) default NULL,
+  `user_newpasswd` varchar(32) default NULL,
+  `user_quickreply` tinyint(1) NOT NULL default '1',
+  `user_token` int(10) unsigned NOT NULL default '0',
+  `user_tokendate` int(10) unsigned NOT NULL default '0',
+  `user_tokenip` varchar(40) default NULL,
+  `user_login_tries` smallint(5) unsigned NOT NULL default '0',
+  `user_last_login_try` int(11) NOT NULL default '0',
+  `user_show_quickreply` tinyint(1) NOT NULL default '1',
+  `user_quickreply_mode` tinyint(1) NOT NULL default '1',
+  `user_open_quickreply` tinyint(1) NOT NULL default '1',
+  PRIMARY KEY  (`user_id`),
+  UNIQUE KEY `user_email` (`user_email`),
+  KEY `user_session_time` (`user_session_time`),
+  KEY `username` (`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -66,4 +134,4 @@ CREATE TABLE `organizations` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2007-09-06  2:21:50
+-- Dump completed on 2007-12-30  1:39:36
