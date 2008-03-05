f58c8770d0cde9af0dfcf087ae4017836b150b07
blast007 <blast007@08b3d480-bf2c-0410-a26f-811ee3361c24>
2008-03-05 07:20:52 +0000

    Add in some horrible upgrade code. Shield your eyes.
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@16689 08b3d480-bf2c-0410-a26f-811ee3361c24

--- web/submitimages/directory.index.php
+++ web/submitimages/directory.index.php
@@ -19,7 +19,7 @@
   
   // Set up the SQLite database object and query the file data for this directory
   
-  define('DATABASE_VERSION', 1);
+  define('DIRECTORY_VERSION', 1);
   
   $filename = $filedirectory.'/data.sqlite3';
   
@@ -39,7 +39,7 @@
       $data['checkVersion'] = $query['checkVersion']->fetch();
   
       // For now, only run further queries if the database version matches   
-      if ($data['checkVersion']['value'] == DATABASE_VERSION)
+      if ($data['checkVersion']['value'] == DIRECTORY_VERSION)
       {
         $query['files'] = $sqlite->query("SELECT * FROM files");
         $data['files'] = $query['files']->fetchAll();
@@ -49,6 +49,90 @@
     {
       //die($exception->getMessage());
     }
+  }
+  else if (file_exists($filedirectory.'/author.txt'))
+  {
+
+    // Try to create an SQLite3 database via the PDO functions
+    try
+    {
+      $sqlite = new PDO('sqlite:'.$filename);
+      // Create the info table, which will store information such as the
+      // database version. (And that's all, so far)
+      $sql = 'CREATE TABLE info(name TEXT PRIMARY KEY, value TEXT)';
+      $sqlite->exec($sql);
+      
+      // Prepare a query that could be used multiple times to insert values
+      // into the info table
+      $query['infoInsert'] = $sqlite->prepare('INSERT INTO info (name, value) VALUES (:name, :value)');
+      $info = Array('name' => false, 'value' => false);
+      $query['infoInsert']->bindParam(':name', $info['name']);
+      $query['infoInsert']->bindParam(':value', $info['value']);
+      
+      // Insert the database version
+      $info['name'] = 'dbversion';
+      $info['value'] = DIRECTORY_VERSION;
+      $query['infoInsert']->execute();
+    
+      // Create the files table. This will store information about the files
+      // contained in a single directory. The filenames will be relative to allow
+      // us to rename directories.
+      
+      $sqlite->exec('CREATE TABLE files(filename TEXT PRIMARY KEY, bzid INTEGER, username TEXT, ipaddress TEXT, uploaderfirstname TEXT, uploaderlastname TEXT, uploaderemail TEXT, filemd5 TEXT, authorname TEXT, licensename TEXT, licenseurl TEXT, licensetext TEXT, moderationmessage TEXT, moderator TEXT)');
+    }
+    catch( PDOException $exception )
+    {
+      //die($exception->getMessage());
+    }
+
+    echo "Upgrading old author.txt to SQLite system<br>";
+    $oldauthorfile = file($filedirectory.'/author.txt');
+    
+    $olduploadername = false;
+    $oldfiles = Array();
+    
+    foreach ($oldauthorfile as $oldline)
+    {
+      if (strlen(trim($oldline)) == 0) continue;
+
+      if (strncmp("Licenses for ", $oldline, strlen("Licenses for ")) == 0)
+      {
+        $olduploadername = explode(" ", substr($oldline, strlen("Licenses for "), strpos($oldline, "'s images.")-strlen("Licenses for "))); 
+      }
+      
+      else if (strncmp("License for ", $oldline, strlen("License for ")) == 0)
+      {
+        $oldfiles[] = Array('filename' => substr($oldline, strlen("License for "), strpos($oldline, ":")-strlen("License for ")),
+                            'license' => substr($oldline, strpos($oldline, ":")+2) );
+      }
+    }
+    
+    if (!$olduploadername)
+      die("This author.txt file was not in the correct format, and cannot be upgraded. Bailing.");
+      
+    if (sizeof($olduploadername) != 2)
+      die("Uploader name was not valid. Bailing.");  
+    
+    echo "Uploader: ".implode(" ", $olduploadername)."<br><pre>";
+    print_r($oldfiles);
+    echo "</pre>";
+    
+    // Prepare a query to insert new entries into the 'files' table
+    $query = $sqlite->prepare('INSERT INTO files (filename, uploaderfirstname, uploaderlastname, filemd5, licensename) VALUES (:filename, :uploaderfirstname, :uploaderlastname, :filemd5, :licensename)');
+
+    $query->bindParam(':uploaderfirstname', $olduploadername[0], PDO::PARAM_STR);
+    $query->bindParam(':uploaderlastname', $olduploadername[1], PDO::PARAM_STR);
+    
+    foreach($oldfiles as $oldfile)
+    {
+      $query->bindParam(':filename', $oldfile['filename'], PDO::PARAM_STR);
+      $query->bindParam(':licensename', $oldfile['license'], PDO::PARAM_STR);
+      if (file_exists($filedirectory.'/'.$oldfile['filename']))
+        $query->bindParam(':filemd5', md5_file($filedirectory.'/'.$oldfile['filename']), PDO::PARAM_STR);
+      $query->execute();
+    }
+    
+    exit;
   }
 
 ?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
@@ -132,7 +216,21 @@
         
         if ($file)
         {
-          echo "    <tr><td><a href=\"$httpdirectory$item\">$item</a></td><td>".nicefilesize($filedirectory."/".$item)."</td><td>".$file['authorname']."</td><td>".$file['uploaderfirstname']." ".$file['uploaderlastname']."</td><td>";
+          echo "    <tr><td><a href=\"$httpdirectory$item\">$item</a></td><td>".nicefilesize($filedirectory."/".$item)."</td><td>";
+          if (strlen($file['authorname']) > 0)
+            echo $file['authorname'];
+          else
+            echo "(Unknown)";
+          
+          echo "</td><td>";
+          
+          if (strlen($file['uploaderfirstname']) > 0 && strlen($file['uploaderlastname']) > 0)
+            echo $file['uploaderfirstname']." ".$file['uploaderlastname'];
+          else
+            echo "(Unknown)";
+
+          echo "</td><td>";
+          
           if ($file['licenseurl'])
             echo '<a href="'.$file['licenseurl'].'">'.$file['licensename']."</a>";
           else
