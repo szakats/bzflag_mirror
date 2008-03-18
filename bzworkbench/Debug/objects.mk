e980cf8c85611e51feeb23278c1ebd328a357077
judecn <judecn@08b3d480-bf2c-0410-a26f-811ee3361c24>
2008-03-18 07:12:21 +0000

    Temporarily removing Debug/, since it will soon be replaced with a different heirarchy.
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@16739 08b3d480-bf2c-0410-a26f-811ee3361c24

--- bzworkbench/Debug/objects.mk
+++
@@ -1,7 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lfltk -lfltk_images -losg -lz -losgViewer -lfltk_gl -lGL -lGLU
