08ff3a8895a3a1ac3ddf4258c6c1d02c8e4c3439
blast007 <blast007@08b3d480-bf2c-0410-a26f-811ee3361c24>
2008-05-10 04:45:27 +0000

    Put the weblogin URL in the action attribute
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@17164 08b3d480-bf2c-0410-a26f-811ee3361c24

--- db/weblogin.php
+++ db/weblogin.php
@@ -121,7 +121,7 @@ function action_weblogin() {
 					<tr>
 						<td valign="top">
 							<table width="40%" border="0" cellpadding="0" cellspacing="0">
-							<form action="" method="POST" >
+							<form action="http://my.bzflag.org/weblogin.php" method="POST" >
 							<tr>
 								<td>Username <INPUT type ="text" name="username"></td>
 							</tr>
