c926612a47ccf75a74d8dfd03f6f9803ce991439
chaos-dev <chaos-dev@08b3d480-bf2c-0410-a26f-811ee3361c24>
2008-06-17 23:08:54 +0000

    Removed unneccessary data and includes and added comments
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@17603 08b3d480-bf2c-0410-a26f-811ee3361c24

--- bzwgen/inc/Generator.h
+++ bzwgen/inc/Generator.h
@@ -9,47 +9,73 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  */
-
+/** 
+ * @file Generator.h
+ * @author Kornel Kisielewicz kornel.kisielewicz@gmail.com
+ * @brief Defines an abstract Generator class for map generation.
+ */
 #ifndef __GENERATOR_H__
 #define __GENERATOR_H__
 
-#include <vector>
-#include <fstream> 
 #include "globals.h"
 #include "RuleSet.h"
 #include "Material.h"
 #include "commandArgs.h"
 
-
+/** 
+ * @class Generator
+ * @brief Abstract class for the map generator. 
+ *
+ * This class is intended to be derived from. It holds a protected RuleSet
+ * and takes care of material handling.
+ */
 class Generator {
 protected:
+  /** Size of the world in world units. */
   int size;
+  /** Pointer to the loaded RuleSet. */
   RuleSet* ruleset;
+  /** A list of all the materials used. */
   MaterialVector mats;
+  /** Number of bases on the map. */
   int bases;
 public:
+  /** Material ID of the standard road material. 
+      Probably shouldn't be public, will be refactored */
   int roadid;
+  /** Material ID of the standard road crossing material. 
+      Probably shouldn't be public, will be refactored */
   int roadxid;
+  /** CTFSafe flag. Also shouldn't be public, will be refactored */
   bool ctfSafe;
-
+  /** Standard constructor, takes a already loaded RuleSet as
+      it's parameter. The ruleset needs to have MATROAD and 
+      MATROADX defined. */
   Generator(RuleSet* _ruleset) : ruleset(_ruleset) {
     roadid  = math::roundToInt(ruleset->getAttr("MATROAD"));
     roadxid = math::roundToInt(ruleset->getAttr("MATROADX"));
   }
+  /** Parse command line or config file options. Virtual so the
+      specific generator can overload it. Generator parses the size,
+      bases and ctfsafe options. */
   virtual void parseOptions(CCommandLineArgs* opt);
+  /** Does nothing, should be overloaded. */
   virtual void run();
+  /** Returns the size of the world. */
   inline int getSize() { return size; }
+  /** Returns the loaded RuleSet. */
   inline RuleSet* getRuleSet() { return ruleset; }
+  /** Outputs data. In case of the Generator class this handles
+      output of the file header and materials. */
   virtual void output(Output& out);
+  /** Returns the Material by material ID. */
   Material* getMaterial(int id) { return mats[id]; }
+  /** Destructor, frees the materials, but not the ruleset. */
   virtual ~Generator() {
     MaterialVectIter itr; 
     for (itr = mats.begin(); itr!= mats.end(); ++itr) delete (*itr);
   }
 };
-
-typedef std::vector<Generator> GeneratorVector;
-typedef GeneratorVector::iterator GeneratorVectIter;
 
 #endif /* __GENERATOR_H__ */
 
