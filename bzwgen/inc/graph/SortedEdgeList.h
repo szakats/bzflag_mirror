94c7f349ab325157fcf8556cd32e168a6152d742
chaos-dev <chaos-dev@08b3d480-bf2c-0410-a26f-811ee3361c24>
2008-07-05 19:03:29 +0000

    2007 -> 2008 geesh
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@17996 08b3d480-bf2c-0410-a26f-811ee3361c24

--- bzwgen/inc/graph/SortedEdgeList.h
+++ bzwgen/inc/graph/SortedEdgeList.h
@@ -1,19 +1,19 @@
 /* bzflag
-* Copyright (c) 1993 - 2007 Tim Riker
-*
-* This package is free software;  you can redistribute it and/or
-* modify it under the terms of the license found in the file
-* named COPYING that should have accompanied this file.
-*
-* THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
-* IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
-* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-*/
+ * Copyright (c) 1993 - 2008 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
 /** 
-* @file SortedEdgeList.h
-* @author Kornel Kisielewicz kornel.kisielewicz@gmail.com
-* @brief Defines an EdgeList class model for the Graph class.
-*/
+ * @file SortedEdgeList.h
+ * @author Kornel Kisielewicz kornel.kisielewicz@gmail.com
+ * @brief Defines an EdgeList class model for the Graph class.
+ */
 
 #ifndef __SORTEDEDGELIST_H__
 #define __SORTEDEDGELIST_H__
