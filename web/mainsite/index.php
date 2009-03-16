2befd4104a742571f023617b33a87bb6bee403ef
brlcad <brlcad@08b3d480-bf2c-0410-a26f-811ee3361c24>
2009-03-16 05:01:21 +0000

    update the main page to utilize the new 'button' images instead of displaying news.  also kill an unclosed table.
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@19443 08b3d480-bf2c-0410-a26f-811ee3361c24

--- web/mainsite/index.php
+++ web/mainsite/index.php
@@ -10,21 +10,31 @@ print <<< end
 <table border="0">
 <tr>
 <td>
-	<div align="center"><b>What is BZFlag:</b></div><br>
+<center><b>BZFlag is a free online multiplayer cross-platform open source 3D tank battle game.</b></center>
+<p>The name originates from "Battle Zone capture the Flag".  It runs on Irix, Linux, *BSD, Windows, Mac OS X, and many other platforms.  It's one of the most popular games ever on Silicon Graphics machines and continues to be developed and improved to this day.  It's one of the most popular open source games ever.  For more information, check out some of the <a href="reviews.php">reviews</a>.</p>
 </td>
 </tr>
 <tr>
-<td>
-BZFlag is a free online multiplayer cross-platform open source 3D tank battle game.  The name originates from "Battle Zone capture the Flag".  It runs on Irix, Linux, *BSD, Windows, Mac OS X, and many other platforms.  It's one of the most popular games ever on Silicon Graphics machines and continues to be developed and improved to this day.  It's one of the most popular open source games ever.  For more information, check out some of the <a href="reviews.php">reviews</a>.
+<td align="center">
+<br><br>
+<a href="http://my.bzflag.org/bb/viewforum.php?f=8" alt="NEWS"><img src="images/bz_news_button.png" width=256 height=128 border=0></a>
+<a href="http://bzflag.org/screenshots/" alt="screenshots"><img src="images/bz_screenshots_button.png" width=256 height=128 border=0></a>
+<a href="https://sourceforge.net/project/showfiles.php?group_id=3248" alt="downloads"><img src="images/bz_downloads_button.png" width=256 height=128 border=0></a>
+<a href="http://my.bzflag.org/bb/" alt="forums"><img src="images/bz_forums_button.png" width=256 height=128 border=0></a>
+<a href="http://bzflag.org/getin" alt="contribute"><img src="images/bz_contribute_button.png" width=256 height=128 border=0></a>
+<a href="http://my.bzflag.org/w/" alt="wiki"><img src="images/bz_wiki_button.png" width=256 height=128 border=0></a>
+
 </td>
 </tr>
 </table>
+
 <br><br>
-<table bgcolor="#EEEEEE" border="0" class="sidebar_border">
-<tr>
-<td>
-<div align="center"><b>Latest News:</b></div><br>
+
 end;
+
+if (0) {
+
+printf("<div align=\"center\"><b>Latest News:</b></div><br>");
 
 if(mysql_num_rows($result) == 0) {
   print "No news!?!?<br><br><br><br>\n";
@@ -36,14 +46,10 @@ if(mysql_num_rows($result) == 0) {
     }
     $i++;
   }
-/*
-  if(mysql_num_rows($result) > 5) {
-    print "<br><div align=\"center\"><a href=\"/bzflag/news/old.php\">old news</a></div>\n</td></tr></table>";
-  }
-*/
 }
 
 mysql_free_result($result);
+}
 
 $doc->end();
 
