d87cc7df961f443713ee927df3ade8293462d0b1
brlcad <brlcad@08b3d480-bf2c-0410-a26f-811ee3361c24>
2009-03-28 19:57:52 +0000

    yes, the hairline border around the sf logo was bothersome.  make it have a 1px black outline like the rest of them.
    git-svn-id: https://bzflag.svn.sourceforge.net/svnroot/bzflag/trunk@19545 08b3d480-bf2c-0410-a26f-811ee3361c24

--- web/mainsite/document.php
+++ web/mainsite/document.php
@@ -90,7 +90,7 @@ print <<< end
 								<td align="center" >
 									<table align="center" border="0" cellspacing="0" cellpadding="0">
 										<tr><td><a href="http://www.opengl.org/"><img src="/images/opengl.gif" alt="opengl" width="88" height="31" border="0"></a></td></tr>
-										<tr><td><a href="http://sourceforge.net/project/?group_id=3248"><img src="http://sourceforge.net/sflogo.php?group_id=3248&amp;type=1" width="88" height="31" alt="sourceforge" border="0"></a></td></tr>
+										<tr><td><a href="http://sourceforge.net/project/?group_id=3248" style="color: #000000;"><img src="http://sourceforge.net/sflogo.php?group_id=3248&amp;type=1" width="86" height="30" alt="sourceforge" border="1"></a></td></tr>
 										<tr><td><a href="http://sourceforge.net/donate/index.php?group_id=3248"><img src="http://images.sourceforge.net/images/project-support.jpg" width="88" height="32" border="0" alt="Support This Project"/></a></td></tr>
 										<tr><td><a href="http://www.linuxgames.com/"><img src="/images/linuxgames.gif" width="88" height="31" alt="linuxgames" border="0"></a></td></tr>
 										<tr><td><a href="http://www.telefragged.com/"><img src="/images/telefragged.gif" width="88" height="31" alt="telefragged" border="0"></a></td></tr>
